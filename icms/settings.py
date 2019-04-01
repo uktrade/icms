f0b786b99552d2b9ee6b64e8d132fc35e1ac1ba0
breneser <b@androsaxon.co.uk>
2019-04-01 13:22:14 +0100

    Add web app

--- icms/settings.py
+++ icms/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xy=wx01yf7#lt%t_sy%75ag(v+g$+go56wur_&nc%_z6jz!w#7'
+SECRET_KEY = os.getenv('ICMS_SECRET_KEY', default=get_random_secret_key )
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
