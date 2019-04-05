038c021c5bbb5a044a9cd4cd20be7bf123d6c28a
breneser <b@androsaxon.co.uk>
2019-04-05 11:51:38 +0100

    Use Gunicorn for service static files in prod

--- icms/wsgi.py
+++ icms/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'icms.settings')
 
-application = get_wsgi_application()
+if settings.DEBUG:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
