91005f885749608b9ad3ddb56a8e9da43ac0aa54
breneser <b@androsaxon.co.uk>
2019-04-05 10:54:37 +0100

    Add Gunicorn for serving app in production

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,7 +1,19 @@
 #!/bin/sh -e
+
+ICMS_WEB_PORT="${ICMS_WEB_PORT:-8080}"
+
+echo "ICMS running now with debug $ICMS_DEBUG"
+
 if [ "${ICMS_MIGRATE:-True}" = 'True' ]; then
   echo "Running migrations"
   python manage.py migrate
 fi
 
-python manage.py runserver 0:"${ICMS_WEB_PORT:-8080}"
+if [ "${ICMS_DEBUG:-False}" = 'True' ]; then
+  python manage.py runserver 0:"$ICMS_WEB_PORT"
+else
+  gunicorn icms.wsgi \
+           --name icms \
+           --workers "${ICMS_NUM_WORKERS:-3}" \
+           --bind 0:"$ICMS_WEB_PORT"
+fi
