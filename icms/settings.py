68512d72400418cc1a2d9d212830cd991a009b6e
breneser <b@androsaxon.co.uk>
2019-04-05 10:54:37 +0100

    Add Gunicorn for serving app in production

--- icms/settings.py
+++ icms/settings.py
@@ -23,7 +23,7 @@ env = environ.Env()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str('ICMS_SECRET_KEY', get_random_secret_key())
 DEBUG = env.bool('ICMS_DEBUG', False)
-ALLOWED_HOSTS = env.list('ICMS_ALLOWED_HOSTS', default=[])
+ALLOWED_HOSTS = env.list('ICMS_ALLOWED_HOSTS', default=['localhost'])
 LOGIN_URL = '/'
 
 # Application definition
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
