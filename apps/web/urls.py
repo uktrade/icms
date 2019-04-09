b8533456535b22168b58cf6b7377dfead55f6dc8
breneser <b@androsaxon.co.uk>
2019-04-09 16:27:02 +0100

    Login form validation

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from django.contrib.auth import views as auth_view
 
 from . import views
+from .forms import LoginForm
 
 urlpatterns = [
     path(
         '',
-        auth_view.LoginView.as_view(template_name='icms/login.html'),
+        auth_view.LoginView.as_view(
+            template_name='icms/login.html', authentication_form=LoginForm),
         name='login'),
     path('logout', views.log_out, name='logout'),
     path('home', views.home, name='home'),
