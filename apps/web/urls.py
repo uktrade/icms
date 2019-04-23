e0c7b5229b56c5993e6fcee73fb71dea634f0836
breneser <b@androsaxon.co.uk>
2019-04-23 17:04:10 +0100

    Add workbasket view and template

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -3,16 +3,24 @@ from django.urls import path, re_path, include
 from django.views import generic
 from django.contrib.auth import views as auth_view
 from viewflow.flow.viewset import FlowViewSet
+from viewflow.flow.views import AllProcessListView
 
 urlpatterns = [
     path(
         '',
-        auth_view.LoginView.as_view(template_name='icms/public/login.html'),
+        auth_view.LoginView.as_view(
+            template_name='icms/public/login.html',
+            redirect_authenticated_user=True),
         name='login'),
     path('logout/', auth_view.LogoutView.as_view(), name='logout'),
     path('home/', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('change_password/', views.change_password, name='change_password'),
+    path(
+        'workbasket/',
+        AllProcessListView.as_view(
+            template_name='icms/internal/workbasket.html'),
+        name='workbasket'),
     path(
         'access/',
         generic.RedirectView.as_view(url='request', permanent=False),
