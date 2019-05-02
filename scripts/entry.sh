e887f259be30bd4349cccdb901fb6351c80effa6
breneser <b@androsaxon.co.uk>
2019-05-02 11:27:33 +0100

    Wait for db before app start

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -3,6 +3,28 @@
 ICMS_WEB_PORT="${ICMS_WEB_PORT:-8080}"
 ICMS_DEBUG="${ICMS_DEBUG:-False}"
 ICMS_NUM_WORKERS="${ICMS_NUM_WORKERS:-3}"
+DATABASE_URL="${DATABASE_URL:-postgres://postgres@db:5432/postgres}"
+
+retry=10
+
+function wait_for_db() {
+  if [ ${retry} -eq 0 ]; then
+    echo "Database access timedout" 1>&2
+    exit 1;
+  fi
+
+  if pg_isready -d "${DATABASE_URL}"; then
+    echo "OK";
+  else
+    retry=$((retry-1))
+    sleep 1;
+    echo "Retrying database access"
+    wait_for_db
+  fi
+}
+
+echo "Waiting for db access"
+wait_for_db
 
 echo "ICMS running now with debug $ICMS_DEBUG"
 
