0029a7140806b7e41e0e8188583a782beb655344
breneser <b@androsaxon.co.uk>
2019-05-17 11:52:38 +0100

    Add phone number validation

--- web/views/validators.py
+++ web/views/validators.py
@@ -1,8 +1,9 @@
+import datetime
 from django.forms import ValidationError
 
 
 def validate_security_answer(form):
-    answer = form.cleaned_data['security_answer']
+    answer = form.cleaned_data.get('security_answer', None)
     if answer != form.user.security_answer:
         raise ValidationError(
             ("Your security answer didn't match the one you gave us when\
@@ -12,7 +13,17 @@ def validate_security_answer(form):
 
 
 def validate_security_answer_confirmation(form):
-    answer = form.cleaned_data['security_answer']
-    repeat = form.cleaned_data['security_answer_repeat']
+    answer = form.cleaned_data.get('security_answer', None)
+    repeat = form.cleaned_data.get('security_answer_repeat', None)
     if answer != repeat:
-        raise ValidationError('Security answers to not match.')
+        raise ValidationError('Security answers do not match.')
+
+    return repeat
+
+
+def validate_date_of_birth(form):
+    date_of_birth = form.cleaned_data.get('date_of_birth', None)
+    if date_of_birth >= datetime.date.today():
+        raise ValidationError("Date of birth can't be in the future")
+
+    return date_of_birth
