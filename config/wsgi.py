0c6a0f446d19e874980cfdc961a60f45319409c7
breneser <b@androsaxon.co.uk>
2019-05-28 15:19:00 +0100

    Fix production app start settings

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.deployment')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 
 application = get_wsgi_application()
