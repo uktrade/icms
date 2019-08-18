76989395efcaf2e6784efa0af22174f55242eede
breneser <b@androsaxon.co.uk>
2019-08-18 12:56:13 +0100

    Remove data migration scripts

--- web/migrations/0028_auto_20190625_1500.py
+++ web/migrations/0028_auto_20190625_1500.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('web', '0027_product_legislation_data'),
+        ('web', '0026_auto_20190625_1354'),
     ]
 
     operations = [
