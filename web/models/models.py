accd50a3bb850b3d3e47d420f42b9865c38e5e52
breneser <b@androsaxon.co.uk>
2019-09-02 14:35:36 +0100

    Add export application

--- web/models/models.py
+++ web/models/models.py
@@ -949,18 +949,19 @@ class ImportApplicationType(models.Model):
         on_delete=models.PROTECT,
         blank=True,
         null=True,
-        related_name='application_types_from')
+        related_name='import_application_types_from')
     consignment_country_group = models.ForeignKey(
         CountryGroup,
         on_delete=models.PROTECT,
         blank=True,
         null=True,
-        related_name='application_types_to')
-    master_country_group = models.ForeignKey(CountryGroup,
-                                             on_delete=models.PROTECT,
-                                             blank=True,
-                                             null=True,
-                                             related_name='application_types')
+        related_name='import_application_types_to')
+    master_country_group = models.ForeignKey(
+        CountryGroup,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        related_name='import_application_types')
     commodity_type = models.ForeignKey(CommodityType,
                                        on_delete=models.PROTECT,
                                        blank=True,
@@ -1355,3 +1356,73 @@ class Mailshot(models.Model):
                                      null=True,
                                      related_name='retracted_mailshots')
     retracted_datetime = models.DateTimeField(blank=False, null=True)
+
+
+class ExportApplicationType(models.Model):
+
+    #  IN_PROGRESS='IN_PROGRESS'
+    #  DELETED = 'DELETED'
+    #  SUBMITTED='SUBMITTED'
+    #  PROCESSING='PROCESSING'
+    #  WITHDRAWN='WITHDRAWN'
+    #  STOPPED='STOPPED'
+    #  COMPLETED='COMPLETED'
+    #  VARIATION
+
+    is_active = models.BooleanField(blank=False, null=False, default=True)
+    type_code = models.CharField(max_length=30, blank=False, null=False)
+    type = models.CharField(max_length=70, blank=False, null=False)
+    allow_multiple_products = models.BooleanField(blank=False, null=False)
+    generate_cover_letter = models.BooleanField(blank=False, null=False)
+    allow_hse_authorization = models.BooleanField(blank=False, null=False)
+    country_group = models.ForeignKey(CountryGroup,
+                                      on_delete=models.PROTECT,
+                                      blank=False,
+                                      null=False)
+    country_group_for_manufacture = models.ForeignKey(
+        CountryGroup,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        related_name='manufacture_export_application_types')
+
+
+class ExportApplication(models.Model):
+
+    is_active = models.BooleanField(blank=False, null=False, default=True)
+    application_type = models.ForeignKey(ExportApplicationType,
+                                         on_delete=models.PROTECT,
+                                         blank=False,
+                                         null=False)
+    submit_datetime = models.DateTimeField(blank=True, null=True)
+    create_datetime = models.DateTimeField(blank=False,
+                                           null=False,
+                                           auto_now_add=True)
+    submitted_by = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        related_name='submitted_export_application')
+    created_by = models.ForeignKey(User,
+                                   on_delete=models.PROTECT,
+                                   blank=False,
+                                   null=False,
+                                   related_name='created_export_applications')
+    exporter = models.ForeignKey(Exporter,
+                                 on_delete=models.PROTECT,
+                                 blank=False,
+                                 null=False,
+                                 related_name='export_applications')
+    exporter_office = models.ForeignKey(
+        Office,
+        on_delete=models.PROTECT,
+        blank=False,
+        null=True,
+        related_name='office_export_applications')
+    contact = models.ForeignKey(User,
+                                on_delete=models.PROTECT,
+                                blank=True,
+                                null=True,
+                                related_name='contact_export_applications')
+    countries = models.ManyToManyField(Country)
