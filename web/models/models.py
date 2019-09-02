85ab1d2d1f08b7b11c1052d3c96f9b95604966ef
breneser <b@androsaxon.co.uk>
2019-09-02 15:13:25 +0100

    Add export case model

--- web/models/models.py
+++ web/models/models.py
@@ -1426,3 +1426,49 @@ class ExportApplication(models.Model):
                                 null=True,
                                 related_name='contact_export_applications')
     countries = models.ManyToManyField(Country)
+
+
+class ExportCase(models.Model):
+    IN_PROGRESS = 'IN_PROGRESS'
+    SUBMITTED = 'SUBMITTED'
+    PROCESSING = 'PROCESSING'
+    COMPLETED = 'COMPLETED'
+    WITHDRAWN = 'WITHDRAWN'
+    STOPPED = 'STOPPED'
+    VARIATION_REQUESTED = 'VARIATION'
+    REVOKED = 'REVOKED'
+    DELETED = 'DELETED'
+
+    STATUSES = ((IN_PROGRESS, 'In Progress'), (SUBMITTED, 'Submitted'),
+                (PROCESSING, 'Processing'), (COMPLETED, 'Completed'),
+                (WITHDRAWN, 'Withdrawn'), (STOPPED, 'Stopped'),
+                (REVOKED, 'Revoked'), (VARIATION_REQUESTED,
+                                       'Case Variation'), (DELETED, 'Deleted'))
+
+    # Decision
+    REFUSE = 'REFUSE'
+    APPROVE = 'APPROVE'
+    DECISIONS = ((REFUSE, 'Refuse'), (APPROVE, 'Approve'))
+
+    application = models.OneToOneField(ExportApplication,
+                                       on_delete=models.PROTECT,
+                                       related_name='case')
+    status = models.CharField(max_length=30,
+                              choices=STATUSES,
+                              blank=False,
+                              null=False)
+    reference = models.CharField(max_length=50, blank=True, null=True)
+    variation_no = models.IntegerField(blank=False, null=False, default=0)
+    decision = models.CharField(max_length=10,
+                                choices=DECISIONS,
+                                blank=True,
+                                null=True)
+    refuse_reason = models.CharField(max_length=4000, blank=True, null=True)
+    last_update_datetime = models.DateTimeField(blank=False,
+                                                null=False,
+                                                auto_now=True)
+    last_updated_by = models.ForeignKey(User,
+                                        on_delete=models.PROTECT,
+                                        blank=False,
+                                        null=False,
+                                        related_name='updated_export_cases')
