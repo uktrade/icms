76acc699e84ca42707fc73ef1de97206ed27374b
breneser <b@androsaxon.co.uk>
2019-10-22 10:52:05 +0100

    Make pagination work outside forms.

--- web/jinja2.py
+++ web/jinja2.py
@@ -11,12 +11,20 @@ def show_all_attrs(value):
     return '\n'.join(res)
 
 
+def modify_query(request, **new_params):
+    params = request.GET.copy()
+    for k, v in new_params.items():
+        params[k] = v
+    return request.build_absolute_uri('?' + params.urlencode())
+
+
 def environment(**options):
     env = Environment(**options)
     env.globals.update({
         'static': staticfiles_storage.url,
         'url': reverse,
-        'get_messages': messages.get_messages
+        'get_messages': messages.get_messages,
+        'modify_query': modify_query
     })
     env.filters['show_all_attrs'] = show_all_attrs
     return env
