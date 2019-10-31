0b403b1191b1190e3bb5844ce8c3a201d81d3d33
breneser <b@androsaxon.co.uk>
2019-10-31 18:06:32 +0000

    Add import application model tests.

---
+++ web/tests/domains/office/factory.py
@@ -0,0 +1,16 @@
+import factory
+import factory.fuzzy
+import random
+from web.domains.office.models import Office
+
+from faker import Faker
+
+fake = Faker('en_GB')
+
+
+class OfficeFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Office
+
+    is_active = random.choice([True, False])
+    address = fake.address()
