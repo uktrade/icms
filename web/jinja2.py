1791f89544d3e49898371afa717e1a66bcc6ffd5
breneser <b@androsaxon.co.uk>
2019-11-05 10:34:21 +0000

    Compress javascript files using django_compressor.

--- web/jinja2.py
+++ web/jinja2.py
@@ -1,3 +1,4 @@
+from compressor.contrib.jinja2ext import CompressorExtension
 from django.contrib import messages
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import reverse
@@ -19,7 +20,7 @@ def modify_query(request, **new_params):
 
 
 def environment(**options):
-    env = Environment(**options)
+    env = Environment(extensions=[CompressorExtension], **options)
     env.globals.update({
         'static': staticfiles_storage.url,
         'url': reverse,
