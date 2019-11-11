061e68107a920bc830122eba7100ab83330a9361
Boris K <borissk@msn.com>
2019-11-11 16:25:19 +0000

    Fix a migration problem

--- web/migrations/0075_user_unsuccessful_login_attempts.py
+++ web/migrations/0075_user_unsuccessful_login_attempts.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('web', '0074_default_user'),
+        ('web', '0073_remove_user_register_complete'),
     ]
 
     operations = [
