f010989361f5a954264ab684477845471891a078
Boris K <borissk@msn.com>
2019-12-19 16:17:31 +0000

    Access request, add JS for showing/hiding elements based on type

---
+++ web/static/web/js/request-access.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function(event) { // doesn't work in IE8: https://caniuse.com/#feat=domcontentloaded
+    'use strict';
+    var reasonRowClasses = getClasses('id_request_reason');
+    var agentNameRowClasses = getClasses('id_agent_name');
+    var agentAddressRowClasses = getClasses('id_agent_address');
+
+    reasonRowClasses.add('hidden');
+    agentNameRowClasses.add('hidden');
+    agentAddressRowClasses.add('hidden');
+
+    var container = document.getElementById('form-container');
+    container.classList.remove('hidden');
+    var containerPos = container.offsetTop;
+
+    var dropdown = document.getElementById('id_request_type');
+    dropdown.onchange = function () {
+        if (dropdown.options.length > 4) {
+            dropdown.remove(0);
+        }
+        switch (dropdown.value) {
+            case 'MAIN_IMPORTER_ACCESS':
+                reasonRowClasses.remove('hidden');
+                agentNameRowClasses.add('hidden');
+                agentAddressRowClasses.add('hidden');
+                container.scrollIntoView(true);
+                break;
+            case 'AGENT_IMPORTER_ACCESS':
+                reasonRowClasses.remove('hidden');
+                agentNameRowClasses.remove('hidden');
+                agentAddressRowClasses.remove('hidden');
+                container.scrollIntoView(true);
+                break;
+            case 'MAIN_EXPORTER_ACCESS':
+                reasonRowClasses.add('hidden');
+                agentNameRowClasses.add('hidden');
+                agentAddressRowClasses.add('hidden');
+                container.scrollIntoView(true);
+                break;
+            case 'AGENT_EXPORTER_ACCESS':
+                reasonRowClasses.add('hidden');
+                agentNameRowClasses.remove('hidden');
+                agentAddressRowClasses.remove('hidden');
+                container.scrollIntoView(true);
+                break;
+        }
+    };
+});
+
+function getClasses(id){
+    'use strict';
+    return document.getElementById(id).parentElement.parentElement.classList;
+}
