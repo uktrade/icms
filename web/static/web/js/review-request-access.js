5ed0c67620324882ba207373c51fb2f072cfeb7e
Boris K <borissk@msn.com>
2020-02-04 16:55:33 +0000

    JS formatting

--- web/static/web/js/review-request-access.js
+++ web/static/web/js/review-request-access.js
@@ -16,17 +16,19 @@ function showModal(event) {
         '<div class="modal-popover small-popover modal-alert-confirm">' +
         '<div class="modal-popover-content" role="alertdialog">' +
         '<div class="modal-popover-icon icon-question"></div>' +
-        '<div class="modal-popover-text">Are you sure you want to close this Access Request? This will email the requester with the status below.' +
-        '</div><ul class="modal-popover-actions"><li><button class="primary-button alert-dismiss" ' +
-        'onclick="javascript:document.body.removeChild(document.body.firstChild);' +
-        'document.getElementsByTagName(\'form\')[0].submit();"' +
-        '>OK</button></li><li><button class="link-button" ' +
-        'onclick="javascript:document.body.removeChild(document.body.firstChild);"' +
-        '>Cancel</button></li></ul></div></div></div>';
+        '<div class="modal-popover-text">Are you sure you want to close this Access Request? This will email the requester with the status below.</div>' +
+        '<ul class="modal-popover-actions"><li><button class="primary-button alert-dismiss" ' +
+        'onclick="javascript:hideModal(); document.getElementsByTagName(\'form\')[0].submit();">OK</button></li><li>' +
+        '<button class="link-button" onclick="hideModal()">Cancel</button></li></ul></div></div></div>';
 
     var div = document.createElement('div');
 
     div.innerHTML = html;
 
     document.body.insertBefore(div, document.body.firstChild);
+}
+
+function hideModal() {
+    "use strict";
+    document.body.removeChild(document.body.firstChild);
 }
