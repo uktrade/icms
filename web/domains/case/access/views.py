56143191b9629eaee46a54038d9cf01db997caaf
Nuno Costa <nuno@francodacosta.com>
2020-02-18 11:32:37 +0000

    FIR  page

--- web/domains/case/access/views.py
+++ web/domains/case/access/views.py
@@ -6,6 +6,8 @@ from django.views.generic.edit import FormView
 from web.utils import SimpleStartFlowMixin, SimpleFlowMixin
 from .forms import AccessRequestForm, ReviewAccessRequestForm
 from .models import AccessRequest, AccessRequestProcess
+from web.domains.case.forms import FurtherInformationRequestDisplayForm
+
 
 def clean_extra_request_data(access_request):
     if access_request.request_type == AccessRequest.IMPORTER:
@@ -68,10 +70,21 @@ class AccessRequestFirListView(TemplateView):
 
     def get_context_data(self, process_id, *args, **kwargs):
         process = AccessRequestProcess.objects.get(pk=process_id)
-
         context = super().get_context_data(*args, **kwargs)
+
+        fir_list = []
+        for fir in process.access_request.further_information_requests.all().order_by('pk').reverse():
+            fir_list.append(FurtherInformationRequestDisplayForm(
+                instance=fir,
+                initial={
+                    'requested_datetime': fir.date_created_formatted().upper(),
+                    'requested_by': fir.requested_by
+                }))
+
+        context['fir_list'] = fir_list
         context['activation'] = {
             'process': process,
         }
 
+        print(context)
         return context
