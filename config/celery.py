06762b294b337963e6d06b52490fe3d5bd369664
breneser <b@androsaxon.co.uk>
2020-04-01 16:25:06 +0100

    Add redis and celery for async tasks
    Sending mailshots will send emails to thousands of users which will be
    done in asynchronously via celery

---
+++ config/celery.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+
+app = Celery('icms')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+
+@app.task
+def debug_task(self):
+    print('Request: {0!r})'.format(self.request))
