a00c030d08afbb059f92e319e5c4080151685350
Nuno Costa <nuno@francodacosta.com>
2020-04-24 13:25:51 +0100

    refactoring

--- web/management/commands/collect-npm.py
+++ web/management/commands/collect-npm.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 
 class Command(BaseCommand):
-    help = 'install js dependencies'
+    help = 'copy js dependencies to the configured location'
 
     def mkdir(self, path):
         if not os.path.isdir(path):
@@ -16,14 +16,12 @@ class Command(BaseCommand):
         self.stdout.write(f'Moving JS Dependencies to {settings.NPM_STATIC_FILES_LOCATION}')
         self.stdout.write(f'using config: {settings.NPM_FILE_PATTERNS}')
 
-        self.mkdir(settings.NPM_STATIC_FILES_LOCATION)
-
         self.stdout.write("\nCopying:")
+        self.mkdir(settings.NPM_STATIC_FILES_LOCATION)
         for base, paths in settings.NPM_FILE_PATTERNS.items():
+            destination = os.path.join(settings.NPM_STATIC_FILES_LOCATION, base)
             for pattern in paths:
                 for file in glob.glob(os.path.join('node_modules', base, pattern)):
-                    destination = os.path.join(settings.NPM_STATIC_FILES_LOCATION, base)
-
                     self.mkdir(destination)
                     print(f"\t{file}\t==>\t{destination}/{os.path.basename(file)}")
                     shutil.copy(file, destination)
