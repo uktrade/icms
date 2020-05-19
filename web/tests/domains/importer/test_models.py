dbc96d95d029984c8e93b0f94d73e3c106e29406
breneser <b@androsaxon.co.uk>
2020-05-19 15:51:11 +0100

    Protect importer management with permissions

---
+++ web/tests/domains/importer/test_models.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from web.domains.importer.models import Importer
+
+
+class ImporterTest(TestCase):
+    def create_importer(
+        self,
+        is_active=True,
+        type=Importer.ORGANISATION,
+        name='Very Best Importers Ltd.',
+    ):
+        return Importer.objects.create(is_active=is_active,
+                                       type=type,
+                                       name=name)
+
+    def test_create_importer(self):
+        importer = self.create_importer()
+        self.assertTrue(isinstance(importer, Importer))
+        self.assertEqual(importer.name, 'Very Best Importers Ltd.')
+        self.assertEqual(importer.type, Importer.ORGANISATION)
+
+    def test_archive_importer(self):
+        importer = self.create_importer()
+        importer.archive()
+        self.assertFalse(importer.is_active)
+
+    def test_unarchive_importer(self):
+        importer = self.create_importer(is_active=False)
+        self.assertFalse(importer.is_active)
+        importer.unarchive()
+        self.assertTrue(importer.is_active)
---
+++ web/tests/domains/importer/test_views.py
@@ -0,0 +1,132 @@
+from web.tests.auth import AuthTestCase
+from web.tests.domains.constabulary.factory import ConstabularyFactory
+from web.tests.domains.importer.factory import ImporterFactory
+
+LOGIN_URL = '/'
+ADMIN_PERMISSIONS = ['IMP_ADMIN:MAINTAIN_ALL:IMP_MAINTAIN_ALL']
+SECTION5_AUTHORITY_PERMISSIONS = [
+    'IMP_EXTERNAL:SECTION5_AUTHORITY_EDITOR:IMP_EDIT_SECTION5_AUTHORITY'
+]
+
+
+class ImporterListViewTest(AuthTestCase):
+    url = '/importer/'
+    redirect_url = f'{LOGIN_URL}?next={url}'
+
+    def test_anonymous_access_redirects(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, self.redirect_url)
+
+    def test_forbidden_access(self):
+        self.login()
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 403)
+
+    def test_admin_access(self):
+        self.login_with_permissions(ADMIN_PERMISSIONS)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_external_user_access(self):
+        self.login_with_permissions(SECTION5_AUTHORITY_PERMISSIONS)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_constabulary_access(self):
+        self.login()
+        constabulary = ConstabularyFactory(is_active=True)
+        constabulary.members.add(self.user)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_page_title(self):
+        self.login_with_permissions(ADMIN_PERMISSIONS)
+        response = self.client.get(self.url)
+        self.assertEqual(response.context_data['page_title'],
+                         'Maintain Importers')
+
+    def test_anonymous_post_access_redirects(self):
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_forbidden_post_access(self):
+        self.login()
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, 403)
+
+    def test_number_of_pages(self):
+        # Create 58 importer as paging lists 50 items per page
+        for i in range(58):
+            ImporterFactory()
+
+        self.login_with_permissions(ADMIN_PERMISSIONS)
+        response = self.client.get(self.url)
+        page = response.context_data['page']
+        self.assertEqual(page.paginator.num_pages, 2)
+
+    def test_page_results(self):
+        for i in range(53):
+            ImporterFactory(is_active=True)
+        self.login_with_permissions(ADMIN_PERMISSIONS)
+        response = self.client.get(self.url + '?page=2')
+        page = response.context_data['page']
+        self.assertEqual(len(page.object_list), 3)
+
+
+class ImporterEditViewTest(AuthTestCase):
+    def setUp(self):
+        super().setUp()
+        self.importer = ImporterFactory()
+        self.url = f'/importer/{self.importer.id}/edit/'
+        self.redirect_url = f'{LOGIN_URL}?next={self.url}'
+
+    def test_anonymous_access_redirects(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, self.redirect_url)
+
+    def test_forbidden_access(self):
+        self.login()
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 403)
+
+    def test_authorized_access(self):
+        self.login_with_permissions(ADMIN_PERMISSIONS)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_page_title(self):
+        self.login_with_permissions(ADMIN_PERMISSIONS)
+        response = self.client.get(self.url)
+        self.assertEqual(response.context_data['page_title'],
+                         f"Editing {self.importer}")
+
+
+class ImporterCreateViewTest(AuthTestCase):
+    def setUp(self):
+        super().setUp()
+        self.importer = ImporterFactory()
+        self.url = f'/importer/new/'
+        self.redirect_url = f'{LOGIN_URL}?next={self.url}'
+
+    def test_anonymous_access_redirects(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, self.redirect_url)
+
+    def test_forbidden_access(self):
+        self.login()
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 403)
+
+    def test_authorized_access(self):
+        self.login_with_permissions(ADMIN_PERMISSIONS)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_page_title(self):
+        self.login_with_permissions(ADMIN_PERMISSIONS)
+        response = self.client.get(self.url)
+        self.assertEqual(response.context_data['page_title'],
+                         'Create Importer')
