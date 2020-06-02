73e6320180cb04d20ef8e5ff19bde3f4851e886f
breneser <b@androsaxon.co.uk>
2020-06-02 11:37:30 +0100

    Revise constabulary access control

--- web/tests/domains/constabulary/test_views.py
+++ web/tests/domains/constabulary/test_views.py
@@ -3,7 +3,7 @@ from web.tests.auth import AuthTestCase
 from .factory import ConstabularyFactory
 
 LOGIN_URL = '/'
-PERMISSIONS = ['IMP_ADMIN:MAINTAIN_ALL:IMP_MAINTAIN_ALL']
+PERMISSIONS = ['IMP_MAINTAIN_ALL']
 
 
 class ConstabularyListViewTest(AuthTestCase):
