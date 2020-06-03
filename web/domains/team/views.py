cbe54afdcb3ac2b72127b1ff29413164ce4a7e8a
breneser <b@androsaxon.co.uk>
2020-06-03 15:42:40 +0100

    Control the list of teams a user sees
    User is only allowed to see teams he is part of as a Resource
    Co-ordinator or Team Coordinator(s)

--- web/domains/team/views.py
+++ web/domains/team/views.py
@@ -1,11 +1,13 @@
+from django.db.models import Q
 from django.urls import reverse_lazy
+
 # from django_filters import CharFilter
 from web.views import ModelFilterView, ModelUpdateView
 from web.views.actions import Edit
 
 from .forms import TeamEditForm, TeamsFilter
 from .mixins import ContactsManagementMixin
-from .models import Team
+from .models import Role, Team
 
 
 class TeamListView(ModelFilterView):
@@ -13,21 +15,33 @@ class TeamListView(ModelFilterView):
     filterset_class = TeamsFilter
     model = Team
     page_title = 'Search Teams'
+    paginate = False
 
     def get_queryset(self):
-        """ Only list teams the user is part of"""
-        """ Super users has access to all"""
-        if self.request.user.is_superuser:
-            return super().get_queryset()
+        """
+            Only list the teams user is part of as 'Resource Co-ordinator'
+            or 'Team Coordinator(s)'.
+            Super users has access to all
+        """
+        user = self.request.user
+        teams_query = super().get_queryset()
+        if user.is_superuser:
+            return teams_query
 
-        return self.request.user.team_set.all()
+        return teams_query.filter(roles__in=Role.objects.filter(
+            group__in=user.groups.filter(
+                Q(name__contains=':Team Coordinator')
+                | Q(name__contains=':Resource Co-ordinator')).all()))
 
     def has_permission(self):
-        """ Allow viewing of teams if user is part of group"""
+        """
+            Allow viewing of teams if user is part of any team as 'Resource Co-ordinator'
+            or 'Team Coordinator(s)'
+        """
         if self.request.user.is_superuser:
             return True
 
-        return self.request.user.groups.count() > 0
+        return self.get_queryset().exists()
 
     class Display:
         fields = ['name']
