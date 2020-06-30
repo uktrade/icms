61582a730a3d0e74b124798bdb77994ad0548ed2
breneser <b@androsaxon.co.uk>
2020-06-30 07:03:54 +0100

    Delete queries for migration as it is no use

--- queries_for_migration/README.md
+++
@@ -1,3 +0,0 @@
-Oracle SQL queries to read/format/transform data into database tables in icms.
-
-These queries are to be used for creating a migration script at a later time.
--- queries_for_migration/reference-data/product-legislation.sql
+++
@@ -1,9 +0,0 @@
---- Product legislation data to migrate over
-SELECT
-  capl.id AS pk
-  , CASE WHEN capl.status='CURRENT' THEN 'True' ELSE 'False' END AS IS_ACTIVE
-  , capl.name
-  , CASE WHEN capl.IS_BIOCIDAL='true' THEN 'True' ELSE 'False' END AS IS_BIOCIDAL
-  , CASE WHEN capl.IS_BIOCIDAL_CLAIM='true' THEN 'True' ELSE 'False' END AS IS_BIOCIDAL_CLAIM
-  , CASE WHEN capl.IS_EU_COSMETICS_REGULATION='true' THEN 'True' ELSE 'False' END AS IS_EU_COSMETICS_REGULATION
-  FROM impmgr.cert_app_product_legislation capl
