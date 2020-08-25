cb69b24e6294802e68f87c4aaa141abd50d5f300
Kévin Etienne <kevin.etienne@digital.trade.gov.uk>
2020-08-25 15:42:00 +0100

    Revert "ICMSLST-414 Use eventlet with gunicorn (#165)" (#176)
    This reverts commit 64115f3098642b8afb979de576095b20272efbb2.
    
    Reverts uktrade/icms#165
    Error was 
    redis.exceptions.ConnectionError: Error -3 connecting to redis:6379. Lookup timed out
    
    Related problem: https://github.com/celery/celery/issues/5924

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -2,6 +2,7 @@
 ICMS_WEB_PORT="${ICMS_WEB_PORT:-8080}"
 ICMS_DEBUG="${ICMS_DEBUG:-False}"
 ICMS_MIGRATE="${ICMS_MIGRATE:-True}"
+ICMS_NUM_WORKERS="${ICMS_NUM_WORKERS:-3}"
 
 echo "ICMS running now with debug $ICMS_DEBUG"
 
@@ -17,5 +18,7 @@ if [ "$ICMS_DEBUG" = 'True' ]; then
   python manage.py runserver 0:"$ICMS_WEB_PORT"
 else
   gunicorn config.wsgi \
-           --config config/gunicorn.py
+           --name icms \
+           --workers "$ICMS_NUM_WORKERS" \
+           --bind 0:"$ICMS_WEB_PORT"
 fi
