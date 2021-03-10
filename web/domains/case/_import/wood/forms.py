e170f4b34dcf10c349f94dfdd83982366fe91d41
Osku Salerma <osku.salerma@digital.trade.gov.uk>
2021-03-10 15:54:33 +0000

    ICMSLST-559 Add editing of wood quota applications (goods).

--- web/domains/case/_import/wood/forms.py
+++ web/domains/case/_import/wood/forms.py
@@ -42,6 +42,23 @@ class PrepareWoodQuotaForm(forms.ModelForm):
 
     exporter_vat_nr = forms.CharField(label="Exporter VAT number")
 
+    commodity_code = forms.ChoiceField(
+        help_text=""" It is the responsibility of the applicant to ensure that
+        the commodity code in this box is correct. If you are unsure of the
+        correct commodity code, consult the HM Revenue and Customs Integrated
+        Tariff Book, Volume 2, which is available from the Stationery Office. If
+        you are still in doubt, contact the Classification Advisory Service on
+        (01702) 366077.""",
+        choices=[(x, x) for x in [None, "4403201110", "4403201910", "4403203110", "4403203910"]],
+    )
+
+    goods_qty = forms.DecimalField(label="Quantity")
+
+    goods_unit = forms.ChoiceField(
+        label="Unit",
+        choices=[(x, x) for x in ["cubic metres"]],
+    )
+
     class Meta:
         model = models.WoodQuotaApplication
         fields = (
@@ -51,6 +68,10 @@ class PrepareWoodQuotaForm(forms.ModelForm):
             "exporter_name",
             "exporter_address",
             "exporter_vat_nr",
+            "commodity_code",
+            "goods_description",
+            "goods_qty",
+            "goods_unit",
         )
 
     def __init__(self, *args, **kwargs):
--- web/domains/case/_import/wood/models.py
+++ web/domains/case/_import/wood/models.py
@@ -8,11 +8,17 @@ class WoodQuotaApplication(ImportApplication):
 
     shipping_year = models.IntegerField(blank=False, null=True)
 
+    # exporter
     exporter_name = models.CharField(max_length=100, blank=False, null=True)
     exporter_address = models.CharField(max_length=4000, blank=False, null=True)
     exporter_vat_nr = models.CharField(max_length=100, blank=False, null=True)
 
+    #  goods
+    commodity_code = models.CharField(max_length=40, blank=False, null=True)
+    goods_description = models.CharField(max_length=100, blank=False, null=True)
+    goods_qty = models.DecimalField(blank=False, null=True, max_digits=9, decimal_places=2)
+    goods_unit = models.CharField(max_length=40, blank=False, null=True)
+
     # TODO: add the fields for the rest of the data:
-    #  ICMSLST-559: goods
     #  ICMSLST-557: certificates/documents
     #  ICMSLST-558: supporting documents
