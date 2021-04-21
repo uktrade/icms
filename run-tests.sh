7a17be70543d06fb5e9431a9d0d918a773990978
Matthew Holmes <matthew.holmes@digital.trade.gov.uk>
2021-04-21 10:27:54 +0100

    ICMSLST-625 Remove unit test coverage task from circleci config.
    Also updated run-tests.sh to not generate coverage report.

--- run-tests.sh
+++ run-tests.sh
@@ -5,4 +5,4 @@ set -e
 export DJANGO_SETTINGS_MODULE=config.settings.test
 export ICMS_DEBUG=False
 
-docker-compose run --rm web pytest --tb=short --cov=web --cov=config --cov-report xml:test-reports/cov.xml --dist=loadfile --tx=4*popen "$@"
+docker-compose run --rm web pytest --tb=short --dist=loadfile --tx=4*popen "$@"
