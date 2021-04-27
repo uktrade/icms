b5483fad26b0b4534bf5d1386a130a9babedf23e
Osku Salerma <osku.salerma@digital.trade.gov.uk>
2021-04-27 08:52:26 +0100

    ICMSLST-602 Add --reuse-db to pytest.ini.
    This speeds up running the tests by 2x.
    
    See the comment in run-tests.sh for how to force re-creation of the
    test database.

--- run-tests.sh
+++ run-tests.sh
@@ -5,4 +5,11 @@ set -e
 export DJANGO_SETTINGS_MODULE=config.settings.test
 export ICMS_DEBUG=False
 
+# we are using this work flow:
+# https://pytest-django.readthedocs.io/en/latest/database.html#example-work-flow-with-reuse-db-and-create-db
+#
+# we have --reuse-db in pytest.ini, so repeated test runs by developers locally
+# are fast. if you change the database schema, you have to do ""./run-test.sh
+# --create-db" to force re-creation of the test database.
+
 docker-compose run --rm web pytest --tb=short --dist=loadfile --tx=4*popen "$@"
