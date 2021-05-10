b12f4c0486601d23e3c64fcb9fb89a7d1787db65
Kévin Etienne <kevin.etienne@digital.trade.gov.uk>
2021-05-10 15:52:17 +0100

    ICMSLST-656 Edit Firearms and Ammunition - Specific Import Licence (SIL)

---
+++ web/domains/case/_import/fa_sil/forms.py
@@ -0,0 +1,116 @@
+from django import forms
+from guardian.shortcuts import get_users_with_perms
+
+from web.domains.country.models import Country
+from web.domains.user.models import User
+
+from . import models
+
+
+class PrepareSILForm(forms.ModelForm):
+    applicant_reference = forms.CharField(
+        label="Applicant's Reference",
+        help_text="Enter your own reference for this application.",
+        required=False,
+    )
+
+    contact = forms.ModelChoiceField(
+        queryset=User.objects.none(),
+        help_text="Select the main point of contact for the case. This will usually be the person who created the application.",
+    )
+
+    section1 = forms.BooleanField(required=False, label="Firearms Licence for")
+    section2 = forms.BooleanField(required=False, label="")
+    section5 = forms.BooleanField(required=False, label="")
+    section58_obsolete = forms.BooleanField(required=False, label="")
+    section58_other = forms.BooleanField(required=False, label="")
+    other_description = forms.CharField(
+        required=False,
+        label="Other Section Description",
+        help_text=(
+            "If you have selected Other in Firearms Act Sections. Please explain why you are making"
+            " this request under this 'Other' section."
+        ),
+    )
+
+    commodity_code = forms.ChoiceField(
+        label="Commodity Code",
+        help_text=(
+            "You must pick the commodity code group that applies to the items that you wish to"
+            ' import. Please note that "ex Chapter 97" is only relevant to collectors pieces and'
+            " items over 100 years old. Please contact HMRC classification advisory service,"
+            " 01702 366077, if you are unsure of the correct code."
+        ),
+        choices=[(x, x) for x in [None, "ex Chapter 93", "ex Chapter 97"]],
+    )
+
+    origin_country = forms.ModelChoiceField(
+        label="Country Of Origin",
+        queryset=Country.objects.filter(country_groups__name="Firearms and Ammunition (SIL) COOs"),
+    )
+
+    consignment_country = forms.ModelChoiceField(
+        label="Country Of Consignment",
+        queryset=Country.objects.filter(country_groups__name="Firearms and Ammunition (SIL) COCs"),
+    )
+
+    additional_comments = forms.CharField(required=False, widget=forms.Textarea(attrs={"rows": 4}))
+
+    class Meta:
+        model = models.SILApplication
+        fields = (
+            "contact",
+            "applicant_reference",
+            "section1",
+            "section2",
+            "section5",
+            "section58_obsolete",
+            "section58_other",
+            "other_description",
+            "origin_country",
+            "consignment_country",
+            "military_police",
+            "eu_single_market",
+            "manufactured",
+            "know_bought_from",
+            "additional_comments",
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        users = get_users_with_perms(
+            self.instance.importer, only_with_perms_in=["is_contact_of_importer"]
+        )
+        self.fields["contact"].queryset = users.filter(is_active=True)
+
+        self.fields["know_bought_from"].required = True
+        self.fields["military_police"].required = True
+        self.fields["eu_single_market"].required = True
+        self.fields["manufactured"].required = True
+
+        yes_no_fields = ["know_bought_from", "military_police", "eu_single_market", "manufactured"]
+        for field in yes_no_fields:
+            # The default label for unknown is "Unknown"
+            self.fields[field].widget.choices = [
+                ("unknown", "---------"),
+                ("true", "Yes"),
+                ("false", "No"),
+            ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        # At least one section should be selected
+        licence_for = ["section1", "section2", "section5", "section58_obsolete", "section58_other"]
+        sections = (cleaned_data.get(section) for section in licence_for)
+        if not any(sections):
+            # The sections are grouped together, error message is added to the last element of the group.
+            # Having empty strings will highlight the fields in red.
+            self.add_error("section1", "")
+            self.add_error("section2", "")
+            self.add_error("section5", "")
+            self.add_error("section58_obsolete", "")
+            self.add_error("section58_other", "You must select at least one item")
+
+        if cleaned_data.get("section58_other") and not cleaned_data.get("other_description"):
+            self.add_error("other_description", "You must enter this item")
--- web/domains/case/_import/fa_sil/views.py
+++ web/domains/case/_import/fa_sil/views.py
@@ -1,9 +1,42 @@
 from django.contrib.auth.decorators import login_required, permission_required
+from django.core.exceptions import PermissionDenied
+from django.db import transaction
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+
+from ..models import ImportApplication
+from . import forms, models
 
 
 @login_required
 @permission_required("web.importer_access", raise_exception=True)
 def edit(request: HttpRequest, pk: int) -> HttpResponse:
-    return render(request, "web/domains/case/import/fa-sil/edit.html", {})
+    with transaction.atomic():
+        application = get_object_or_404(models.SILApplication.objects.select_for_update(), pk=pk)
+
+        task = application.get_task(ImportApplication.IN_PROGRESS, "prepare")
+
+        if not request.user.has_perm("web.is_contact_of_importer", application.importer):
+            raise PermissionDenied
+
+        if request.POST:
+            form = forms.PrepareSILForm(data=request.POST, instance=application)
+
+            if form.is_valid():
+                form.save()
+
+                return redirect(reverse("import:fa-sil:edit", kwargs={"pk": pk}))
+
+        else:
+            form = forms.PrepareSILForm(instance=application, initial={"contact": request.user})
+
+        context = {
+            "process_template": "web/domains/case/import/partials/process.html",
+            "process": application,
+            "task": task,
+            "form": form,
+            "page_title": "Firearms and Ammunition (Specific Import Licence) - Edit",
+        }
+
+        return render(request, "web/domains/case/import/fa-sil/edit.html", context)
