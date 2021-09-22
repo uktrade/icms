5af7d0dfb10f0704c125d7285c32a509af246d44
Kévin Etienne <kevin.etienne@digital.trade.gov.uk>
2021-09-22 12:28:14 +0100

    Feedback

--- web/tests/domains/case/test_models.py
+++ web/tests/domains/case/test_models.py
@@ -10,21 +10,19 @@ from web.domains.case.models import ApplicationBase, UpdateRequest, WithdrawAppl
 from web.domains.workbasket.base import WorkbasketAction
 from web.flow.models import Task
 
-pytestmark = pytest.mark.django_db
-
 
 @pytest.fixture
-def app_in_progress(importer, test_import_user):
+def app_in_progress(db, importer, test_import_user):
     return _create_wood_app(importer, test_import_user, ApplicationBase.Statuses.IN_PROGRESS)
 
 
 @pytest.fixture
-def app_submitted(importer, test_import_user):
+def app_submitted(db, importer, test_import_user):
     return _create_wood_app(importer, test_import_user, ApplicationBase.Statuses.SUBMITTED)
 
 
 @pytest.fixture
-def app_processing(importer, test_import_user, test_icms_admin_user):
+def app_processing(db, importer, test_import_user, test_icms_admin_user):
     app = _create_wood_app(
         importer,
         test_import_user,
@@ -38,31 +36,31 @@ def app_processing(importer, test_import_user, test_icms_admin_user):
 
 
 @pytest.fixture
-def app_completed(importer, test_import_user):
+def app_completed(db, importer, test_import_user):
     return _create_wood_app(importer, test_import_user, ApplicationBase.Statuses.COMPLETED)
 
 
 @pytest.fixture
-def completed(importer, test_import_user):
+def completed(db, importer, test_import_user):
     _create_wood_app(importer, test_import_user, ApplicationBase.Statuses.COMPLETED)
 
 
 def test_actions_in_progress(app_in_progress, test_import_user):
     user_row = app_in_progress.get_workbasket_row(test_import_user)
 
-    _check_actions(user_row.actions, expected_names={"Resume", "Cancel"})
+    _check_actions(user_row.actions, expected_actions={"Resume", "Cancel"})
 
 
 def test_actions_submitted(app_submitted, test_import_user):
     user_row = app_submitted.get_workbasket_row(test_import_user)
 
-    _check_actions(user_row.actions, expected_names={"Request Withdrawal", "View"})
+    _check_actions(user_row.actions, expected_actions={"Request Withdrawal", "View"})
 
 
 def test_actions_processing(app_processing, test_import_user):
     user_row = app_processing.get_workbasket_row(test_import_user)
 
-    _check_actions(user_row.actions, expected_names={"Request Withdrawal", "View"})
+    _check_actions(user_row.actions, expected_actions={"Request Withdrawal", "View"})
 
 
 def test_actions_fir_withdrawal_update_request(app_processing, test_import_user):
@@ -71,7 +69,7 @@ def test_actions_fir_withdrawal_update_request(app_processing, test_import_user)
 
     _check_actions(
         user_row.actions,
-        expected_names={"Pending Withdrawal", "View", "Respond FIR"},
+        expected_actions={"Pending Withdrawal", "View", "Respond FIR"},
     )
 
     _create_update_request(app_processing)
@@ -79,7 +77,7 @@ def test_actions_fir_withdrawal_update_request(app_processing, test_import_user)
 
     _check_actions(
         user_row.actions,
-        expected_names={"Pending Withdrawal", "View", "Respond FIR", "Respond to Update Request"},
+        expected_actions={"Pending Withdrawal", "View", "Respond FIR", "Respond to Update Request"},
     )
 
 
@@ -87,7 +85,7 @@ def test_actions_authorise(app_processing, test_import_user):
     _update_task(app_processing, Task.TaskType.AUTHORISE)
     user_row = app_processing.get_workbasket_row(test_import_user)
 
-    _check_actions(user_row.actions, expected_names={"Request Withdrawal", "View"})
+    _check_actions(user_row.actions, expected_actions={"Request Withdrawal", "View"})
 
 
 @override_settings(ALLOW_BYPASS_CHIEF_NEVER_ENABLE_IN_PROD=True)
@@ -95,25 +93,25 @@ def test_actions_bypass_chief(app_processing, test_import_user):
     _update_task(app_processing, Task.TaskType.CHIEF_WAIT)
     user_row = app_processing.get_workbasket_row(test_import_user)
 
-    _check_actions(user_row.actions, expected_names={"Request Withdrawal", "View"})
+    _check_actions(user_row.actions, expected_actions={"Request Withdrawal", "View"})
 
     _update_task(app_processing, Task.TaskType.CHIEF_ERROR)
     user_row = app_processing.get_workbasket_row(test_import_user)
 
-    _check_actions(user_row.actions, expected_names={"Request Withdrawal", "View"})
+    _check_actions(user_row.actions, expected_actions={"Request Withdrawal", "View"})
 
 
 def test_actions_acknowledge(app_processing, test_import_user):
     _update_task(app_processing, Task.TaskType.ACK)
     user_row = app_processing.get_workbasket_row(test_import_user)
 
-    _check_actions(user_row.actions, expected_names={"Request Withdrawal", "View"})
+    _check_actions(user_row.actions, expected_actions={"Request Withdrawal", "View"})
 
 
 def test_actions_completed(app_completed, test_import_user):
     user_row = app_completed.get_workbasket_row(test_import_user)
 
-    _check_actions(user_row.actions, expected_names={"Acknowledge Notification", "View"})
+    _check_actions(user_row.actions, expected_actions={"Acknowledge Notification", "View"})
 
 
 @override_settings(DEBUG_SHOW_ALL_WORKBASKET_ROWS=False)
@@ -127,14 +125,14 @@ def test_admin_actions_in_progress_ilb_admin(app_in_progress, test_icms_admin_us
 def test_admin_actions_submitted(app_submitted, test_icms_admin_user):
     admin_row = app_submitted.get_workbasket_row(test_icms_admin_user)
 
-    _check_actions(admin_row.actions, expected_names={"Take Ownership", "View"})
+    _check_actions(admin_row.actions, expected_actions={"Take Ownership", "View"})
 
 
 @override_settings(DEBUG_SHOW_ALL_WORKBASKET_ROWS=False)
 def test_admin_actions_processing(app_processing, test_icms_admin_user):
     admin_row = app_processing.get_workbasket_row(test_icms_admin_user)
 
-    _check_actions(admin_row.actions, expected_names={"Manage"})
+    _check_actions(admin_row.actions, expected_actions={"Manage"})
 
 
 @override_settings(DEBUG_SHOW_ALL_WORKBASKET_ROWS=False)
@@ -144,7 +142,7 @@ def test_admin_actions_fir_withdrawal_update_request(
     _create_fir_withdrawal(app_processing, test_import_user)
     admin_row = app_processing.get_workbasket_row(test_icms_admin_user)
 
-    _check_actions(admin_row.actions, expected_names={"Manage"})
+    _check_actions(admin_row.actions, expected_actions={"Manage"})
 
     _create_update_request(app_processing)
     admin_row = app_processing.get_workbasket_row(test_icms_admin_user)
@@ -158,7 +156,7 @@ def test_admin_actions_authorise(app_processing, test_icms_admin_user):
     admin_row = app_processing.get_workbasket_row(test_icms_admin_user)
 
     _check_actions(
-        admin_row.actions, expected_names={"Cancel Authorisation", "Authorise Documents", "View"}
+        admin_row.actions, expected_actions={"Cancel Authorisation", "Authorise Documents", "View"}
     )
 
 
@@ -174,7 +172,7 @@ def test_admin_actions_bypass_chief(app_processing, test_icms_admin_user):
 
     _check_actions(
         admin_row.actions,
-        expected_names={
+        expected_actions={
             "(TEST) Bypass CHIEF induce failure",
             "(TEST) Bypass CHIEF",
             "Monitor Progress",
@@ -185,7 +183,7 @@ def test_admin_actions_bypass_chief(app_processing, test_icms_admin_user):
     _update_task(app_processing, Task.TaskType.CHIEF_ERROR)
     admin_row = app_processing.get_workbasket_row(test_icms_admin_user)
 
-    _check_actions(admin_row.actions, expected_names={"Show Licence Details", "View"})
+    _check_actions(admin_row.actions, expected_actions={"Show Licence Details", "View"})
 
 
 @override_settings(DEBUG_SHOW_ALL_WORKBASKET_ROWS=False)
@@ -200,15 +198,15 @@ def test_admin_actions_acknowledge(app_processing, test_icms_admin_user):
 def test_admin_actions_completed(app_completed, test_icms_admin_user):
     admin_row = app_completed.get_workbasket_row(test_icms_admin_user)
 
-    _check_actions(admin_row.actions, expected_names={"View"})
+    _check_actions(admin_row.actions, expected_actions={"View"})
 
 
-def _check_actions(actions: list[list[WorkbasketAction]], expected_names: set[str]):
+def _check_actions(actions: list[list[WorkbasketAction]], expected_actions: set[str]):
     # Only one set of actions
     assert len(actions) == 1, f"One action expected but {len(actions)} passed"
 
     row_actions = {r.name for r in actions[0]}
-    assert row_actions == expected_names
+    assert row_actions == expected_actions
 
 
 def _create_fir_withdrawal(app, test_import_user):
