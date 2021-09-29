f9d6b5394505abfe545d417419d0007292f23dcb
Osku Salerma <osku.salerma@digital.trade.gov.uk>
2021-09-29 13:54:06 +0100

    ICMSLST-643 Add middleware that prints all DB queries made.
    Enable it by setting SHOW_DB_QUERIES=True in your .env file.

--- web/middleware/common.py
+++ web/middleware/common.py
@@ -1,3 +1,4 @@
+from web.utils.db import show_queries
 from web.utils.lock_manager import LockManager
 
 
@@ -20,3 +21,17 @@ class ICMSMiddlewareContext:
 
     def __init__(self):
         self.lock_manager = LockManager()
+
+
+class DBQueriesMiddleware:
+    """Prints all database queries made."""
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+
+        show_queries(0, show_time=True, fancy_print=True)
+
+        return response
