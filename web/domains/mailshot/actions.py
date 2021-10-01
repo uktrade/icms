846c342a3b13c10a5fadf5f600055609ba2959e3
Kévin Etienne <kevin.etienne@digital.trade.gov.uk>
2021-10-01 15:50:07 +0100

    Feedback

--- web/domains/mailshot/actions.py
+++ web/domains/mailshot/actions.py
@@ -6,26 +6,26 @@ from .models import Mailshot
 
 
 class Edit(actions.Edit):
-    def display(self, mailshot):
+    def display(self, mailshot: Mailshot) -> bool:
         return mailshot.status == Mailshot.Statuses.DRAFT
 
 
 class View(actions.View):
-    def display(self, mailshot):
+    def display(self, mailshot: Mailshot) -> bool:
         return mailshot.status != Mailshot.Statuses.DRAFT
 
 
 class ViewReceived(View):
-    def href(self, object):
-        return reverse("mailshot-detail-received", kwargs={"mailshot_pk": object.pk})
+    def href(self, mailshot: Mailshot) -> str:
+        return reverse("mailshot-detail-received", kwargs={"mailshot_pk": mailshot.pk})
 
 
 class Retract(actions.Edit):
     icon = "icon-bin"
     label = "Retract"
 
-    def href(self, object):
-        return reverse("mailshot-retract", kwargs={"mailshot_pk": object.pk})
+    def href(self, mailshot: Mailshot) -> str:
+        return reverse("mailshot-retract", kwargs={"mailshot_pk": mailshot.pk})
 
-    def display(self, mailshot):
+    def display(self, mailshot: Mailshot) -> bool:
         return mailshot.status == Mailshot.Statuses.PUBLISHED
