e64db2f409fbf4b57e96e9ee80947f968bfb9bef
Kévin Etienne <kevin.etienne@digital.trade.gov.uk>
2021-10-15 15:15:20 +0100

    ICMSLST-1129 Add field to filter archived SanctionEmails

--- web/domains/sanction_email/filters.py
+++ web/domains/sanction_email/filters.py
@@ -1,6 +1,14 @@
+from typing import TYPE_CHECKING
+
 import django_filters
+from django.forms.widgets import CheckboxInput
+
+from web.domains.mailshot.models import Mailshot
 
 from . import models
+
+if TYPE_CHECKING:
+    from django.db.models import QuerySet
 
 
 class SanctionEmailsFilter(django_filters.FilterSet):
@@ -9,6 +17,26 @@ class SanctionEmailsFilter(django_filters.FilterSet):
         field_name="email", lookup_expr="icontains", label="Email Address"
     )
 
+    is_archived = django_filters.BooleanFilter(
+        field_name="is_active",
+        lookup_expr="exact",
+        exclude=True,  # if checked get non active
+        widget=CheckboxInput,
+        label="Search Archived",
+    )
+
     class Meta:
         model = models.SanctionEmail
-        fields = ["name", "email"]
+        fields = ["name", "email", "is_archived"]
+
+    @property
+    def qs(self) -> "QuerySet[Mailshot]":
+        qs = super().qs
+
+        # initial state - filter form not submitted
+        if not self.data:
+            return qs.filter(is_active=True)
+
+        # filter form submitted - fields will alter the query
+        else:
+            return qs
