41011311623a85b0848bb69959fd10118991acea
Matthew Holmes <matthew.holmes@digital.trade.gov.uk>
2021-12-10 14:31:40 +0000

    ICMSLST-1244 Display workbasket when logging in to ICMS.

---
+++ web/tests/auth/test_views.py
@@ -0,0 +1,17 @@
+from django.urls import reverse
+from pytest_django.asserts import assertInHTML, assertRedirects
+
+
+def test_login_redirects_to_workbasket(client, test_icms_admin_user):
+    url = reverse("login")
+
+    resp = client.post(url, data={"username": "Unknown", "password": "password"})
+
+    assert resp.status_code == 200
+    assertInHTML(
+        '<div id="login-error">Invalid username or password.<br/>N.B passwords are case sensitive</div>',
+        resp.content.decode("utf-8"),
+    )
+
+    resp = client.post(url, {"username": test_icms_admin_user.username, "password": "test"})
+    assertRedirects(resp, reverse("workbasket"), 302)
