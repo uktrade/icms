d35a42479c02059828f47299321bb1327762a2cc
David Buxton <david.buxton@digital.trade.gov.uk>
2022-01-21 10:27:57 +0000

    ICMSLST-1388 New command "./manage.py countries export" (#708)
    * ICMSLST-1388 New command "./manage.py countries export"
    
    The command writes the country records and country group names to <stdout>
    in CSV format. Where a country belongs to a group, there is a "Y" in the
    cell (the group names are the column headers).
    
    Use this like "./manage.py countries export > report.csv" to save the
    output to a file named "report.csv".
    
    There is also a "./manage.py countries import" command that reads from
    <stdin>, but all it does at the moment is print how many countries and
    groups were in the CSV.

---
+++ web/management/commands/countries.py
@@ -0,0 +1,107 @@
+import argparse
+import csv
+import operator
+import sys
+from typing import Any, Iterable, TextIO
+
+from django.core.management.base import BaseCommand
+from django.db.models import QuerySet
+
+from web.domains.country.models import Country, CountryGroup
+
+# The column / field order is also used to check we have a valid-looking input
+# when reading a CSV and loading data.
+COUNTRY_FIELDS = ["id", "name", "is_active", "type", "commission_code", "hmrc_code"]
+# Marker in a spreadsheet cell to indicate group membership.
+MEMBERSHIP = "Y"
+
+
+class Command(BaseCommand):
+    help = "Import countries data from <stdin> or export to <stdout>"
+
+    def add_arguments(self, parser: argparse.ArgumentParser):
+        parser.add_argument(
+            "operation", choices=["import", "export"], help="Import or export countries data"
+        )
+
+    def handle(self, *args, **options):
+        if options["operation"] == "export":
+            countries, groups = get_country_data()
+            write_country_csv(countries, groups, dest=self.stdout)
+            self.stderr.write(f"Wrote CSV data to {self.stdout}")
+        elif options["operation"] == "import":
+            self.stderr.write(f"Reading CSV data from {sys.stdin}")
+            countries, groups = read_country_csv(src=sys.stdin)
+            self.stderr.write(f"Summary: {len(groups)} groups, {len(countries)} countries.")
+            self.stderr.write("No data was imported (not implemented).")
+
+
+def get_country_data() -> tuple[QuerySet[Country], QuerySet[CountryGroup]]:
+    countries = Country.objects.prefetch_related("country_groups").all()
+    groups = CountryGroup.objects.all()
+
+    return countries, groups
+
+
+def write_country_csv(
+    countries: Iterable[Country], groups: Iterable[CountryGroup], dest: TextIO = sys.stdout
+):
+    getter = operator.attrgetter(*COUNTRY_FIELDS)
+    groupnames = sorted(g.name for g in groups)
+    # First few column names are country fields, followed by a column for
+    # each of the named groups (there are more than a dozen groups).
+    fieldnames = COUNTRY_FIELDS + groupnames
+
+    writer = csv.DictWriter(dest, fieldnames)
+    writer.writeheader()
+
+    # Country group comments immediately below the column headers.
+    row = {group.name: group.comments for group in groups}
+    writer.writerow(row)
+
+    for country in countries:
+        row = dict(zip(COUNTRY_FIELDS, getter(country)))
+        # Add a mark for each group this country belongs to.
+        row.update({group.name: MEMBERSHIP for group in country.country_groups.all()})
+        writer.writerow(row)
+
+
+def read_country_csv(src: TextIO = sys.stdin) -> tuple[list[dict[str, Any]], list[dict[str, str]]]:
+    reader = csv.reader(src)
+    header_start_len = len(COUNTRY_FIELDS)
+
+    groups = []
+    groupnames = []
+    countries = []
+
+    # Ignore any rows at the beginning until we find a valid header. N.B. CSV
+    # reader returns rows as list (not tuple).
+    for row in reader:
+        if row[:header_start_len] == COUNTRY_FIELDS:
+            # Nice. We found the header row. The country group names are the
+            # remaining column values in this row. Following row is comments for
+            # the groups, after which we get the countries and membership.
+            groupnames = row[header_start_len:]
+            try:
+                comments = next(reader)[header_start_len:]
+            except StopIteration:
+                # No more rows! But we need empty comments for the zip below.
+                comments = [""] * len(groupnames)
+
+            for name, comment in zip(groupnames, comments):
+                groups.append({"name": name, "comments": comment})
+
+            break
+    else:
+        # We went through every row, no header found.
+        raise ValueError(f"Missing header, expected columns {COUNTRY_FIELDS}")
+
+    # Remaining rows are country data.
+    for row in reader:
+        country: dict[str, Any] = dict(zip(COUNTRY_FIELDS, row[:header_start_len]))
+        member = dict(zip(groupnames, row[header_start_len:]))
+        country["groups"] = [k for k, v in member.items() if v == MEMBERSHIP]
+
+        countries.append(country)
+
+    return countries, groups
---
+++ web/tests/management/commands/test_countries.py
@@ -0,0 +1,120 @@
+import csv
+import io
+from unittest import mock
+
+import pytest
+from django.core.management import call_command
+
+from web.domains.country.models import Country, CountryGroup
+from web.management.commands.countries import read_country_csv, write_country_csv
+
+
+class TestReadCountryCSV:
+    def test_read(self):
+        rows = [
+            ["first", "row", "is" "junk"],
+            ["id", "name", "is_active", "type", "commission_code", "hmrc_code", "foo", "bar"],
+            ["", "", "", "", "", "", "comment on foo", ""],
+            ["a", "Mexico", "true", "X", "abc", "def", "Y", ""],
+            ["b", "Canada", "false", "Y", "ghi", "jkl", "", "Y"],
+        ]
+        fh = io.StringIO()
+        csv.writer(fh).writerows(rows)
+        fh.seek(0)
+        countries, groups = read_country_csv(fh)
+
+        assert countries == [
+            {
+                "commission_code": "abc",
+                "groups": ["foo"],
+                "hmrc_code": "def",
+                "id": "a",
+                "is_active": "true",
+                "name": "Mexico",
+                "type": "X",
+            },
+            {
+                "commission_code": "ghi",
+                "groups": ["bar"],
+                "hmrc_code": "jkl",
+                "id": "b",
+                "is_active": "false",
+                "name": "Canada",
+                "type": "Y",
+            },
+        ]
+        assert groups == [
+            {"name": "foo", "comments": "comment on foo"},
+            {"name": "bar", "comments": ""},
+        ]
+
+    def test_read_headers_but_no_comments(self):
+        rows = [
+            ["id", "name", "is_active", "type", "commission_code", "hmrc_code", "foo"],
+        ]
+        fh = io.StringIO()
+        csv.writer(fh).writerows(rows)
+        fh.seek(0)
+        countries, groups = read_country_csv(fh)
+
+        assert countries == []
+        assert groups == [{"name": "foo", "comments": ""}]
+
+    def test_read_bad_csv_format(self):
+        rows = [["foo", "bar", "baz"]]
+        fh = io.StringIO()
+        csv.writer(fh).writerows(rows)
+        fh.seek(0)
+
+        with pytest.raises(ValueError, match=r"Missing header, expected columns"):
+            read_country_csv(fh)
+
+
+class TestWriteCountryCSV:
+    def test_write_column_header(self):
+        fh = io.StringIO()
+        countries = []
+        groups = [CountryGroup(name="foo", comments="comment"), CountryGroup(name="bar")]
+        write_country_csv(countries, groups, dest=fh)
+
+        assert fh.getvalue() == (
+            "id,name,is_active,type,commission_code,hmrc_code,bar,foo\r\n" ",,,,,,,comment\r\n"
+        )
+
+    @pytest.mark.django_db
+    def test_write_group_membership(self):
+        fh = io.StringIO()
+        foo = Country.objects.create(name="Foo")
+        bar = CountryGroup.objects.create(name="Bar")
+        bar.countries.add(foo)
+
+        write_country_csv([foo], [bar], dest=fh)
+
+        assert fh.getvalue() == (
+            f"id,name,is_active,type,commission_code,hmrc_code,Bar\r\n"
+            f",,,,,,\r\n"
+            f"{foo.pk},Foo,True,,,,Y\r\n"
+        )
+
+
+class TestManagementCommand:
+    @pytest.mark.django_db
+    def test_call_countries_export(self, capsys):
+        # We have ~180 countries loaded as initial db migrations, but foreign
+        # key constraints make it tedious to delete those. So let's just test
+        # the CSV was written and has the column headers.
+        call_command("countries", "export")
+        captured = capsys.readouterr()
+        first_row = next(csv.reader(io.StringIO(captured.out)))
+
+        assert first_row[:6] == ["id", "name", "is_active", "type", "commission_code", "hmrc_code"]
+
+    def test_call_countries_import(self, capsys):
+        fh = io.StringIO("id,name,is_active,type,commission_code,hmrc_code,Bar\r\n")
+
+        with mock.patch("sys.stdin", fh):
+            call_command("countries", "import")
+
+        captured = capsys.readouterr()
+
+        assert "Summary: 1 groups, 0 countries" in captured.err
