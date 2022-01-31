9609bf2fa9bb796bc51059460ee6275155a4213b
Michael Lockwood <michael.lockwood@digital.trade.gov.uk>
2022-01-31 15:29:59 +0000

    ICMSLST-1379 Set up app for data migration

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -2,6 +2,20 @@
 ICMS_WEB_PORT="${ICMS_WEB_PORT:-8080}"
 ICMS_DEBUG="${ICMS_DEBUG:-False}"
 ICMS_MIGRATE="${ICMS_MIGRATE:-True}"
+APP_ENV="${APP_ENV:-dev}"
+ALLOW_DATA_MIGRATION="${ALLOW_DATA_MIGRATION:-False}"
+
+if [ "$ALLOW_DATA_MIGRATION" = 'True' ] && [ "$APP_ENV" = 'production' ]; then
+  echo "Creating oracle directory"
+  mkdir -p oracle
+  echo "Downloading zipped oracle instant client"
+  curl -L https://download.oracle.com/otn_software/linux/instantclient/215000/instantclient-basiclite-linux.x64-21.5.0.0.0dbru.zip \
+    -o oracle/instantclient-basiclite-linux.x64-21.5.0.0.0dbru.zip
+  echo "Unzipping oracle instant client"
+  unzip oracle/instantclient-basiclite-linux.x64-21.5.0.0.0dbru.zip -d oracle
+  echo "Removing zipped oracle instant client"
+  rm oracle/instantclient-basiclite-linux.x64-21.5.0.0.0dbru.zip
+fi
 
 echo "ICMS running now with debug $ICMS_DEBUG"
 
