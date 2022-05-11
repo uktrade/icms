9fd0d6d68ffe81196595b84a71668adf0e7187e4
Matthew Holmes <matthew.holmes@digital.trade.gov.uk>
2022-05-11 09:03:39 +0100

    ICMSLST-1575 Fix optional label always showing on optional forms.

--- web/forms/mixins.py
+++ web/forms/mixins.py
@@ -20,7 +20,13 @@ class OptionalFormMixin:
     """Makes all form fields optional to allow partial saving."""
 
     def __init__(self, *args, **kwargs):
+        # Keep a track of the required fields before making them optional
+        self.required_fields: list[str] = []
+
         super().__init__(*args, **kwargs)  # type: ignore[call-arg]
 
         for f in self.fields:  # type: ignore[attr-defined]
+            if self.fields[f].required:  # type: ignore[attr-defined]
+                self.required_fields.append(f)
+
             self.fields[f].required = False  # type: ignore[attr-defined]
