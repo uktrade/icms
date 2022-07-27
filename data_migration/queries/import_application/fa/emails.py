671b6a99285f6c40cfd0b7059bd33f076e545f7c
Michael Lockwood <michael.lockwood@digital.trade.gov.uk>
2022-07-27 12:55:34 +0100

    ICMSLST-1690 Import Application Case Email Data Migration

---
+++ data_migration/queries/import_application/fa/emails.py
@@ -0,0 +1,17 @@
+__all__ = ["constabulary_emails"]
+
+constabulary_emails = """
+SELECT
+  imad_id
+  , ce.email_status status
+  , cd.email_address "to"
+  , ce.email_cc_address_list cc_address_list_str
+  , ce.email_subject subject
+  , ce.email_body body
+  , ce.email_response response
+  , ce.email_sent_datetime sent_datetime
+  , ce.email_closed_datetime closed_datetime
+FROM impmgr.xview_ima_constabulary_emails ce
+LEFT JOIN Impmgr.xview_constabulary_details cd ON cd.con_id = ce.email_constabulary AND cd.status_control = 'C'
+WHERE ce.status_control = 'C'
+"""
