62fa7131aa7b1ef3fa269be5ae3a6f2dfc54248d
Matthew Holmes <matthew.holmes@digital.trade.gov.uk>
2022-09-30 16:53:07 +0100

    ICMSLST-1749 Add endpoint in ICMS to process CHIEF licence reply data.

--- web/domains/chief/client.py
+++ web/domains/chief/client.py
@@ -5,8 +5,10 @@ from urllib.parse import urljoin
 import mohawk
 import requests
 from django.conf import settings
+from django.utils import timezone
 
-from web.domains.case._import.models import LiteChiefReference
+from web.domains.case._import.models import LiteHMRCChiefRequest
+from web.domains.case.shared import ImpExpStatus
 from web.domains.chief.views import seen_nonce
 from web.flow.models import ProcessTypes, Task
 from web.utils.sentry import capture_exception
@@ -93,25 +95,44 @@ def send_application_to_chief(
 ) -> None:
     """Sends licence data to CHIEF if enabled."""
 
-    chief_ref = None
+    chief_req = None
     next_task = Task.TaskType.CHIEF_WAIT
 
     try:
         if settings.SEND_LICENCE_TO_CHIEF:
-            # For now use get_or_create (although we will need to revisit this when doing updates)
-            chief_ref, _ = LiteChiefReference.objects.get_or_create(
-                import_application=application, case_reference=application.reference
+            # TODO: Revisit when implementing ICMSLST-1762
+            if application.status == ImpExpStatus.VARIATION_REQUESTED:
+                # Having looked at lite-hmrc we need to create a new lite_hmrc_id each time.
+                # CHIEF licence ingest view
+                # if data["action"] == LicenceActionEnum.UPDATE:
+                #     data["old_reference"] = LicenceIdMapping.objects.get(lite_id=data["old_id"]).reference
+
+                # 1. get lite_chief_id from the latest LiteHMRCChiefRequests record for this application.
+                # 2. Create a new record and serialise the data
+                # 3. Include the old lite_chief_id in the request data
+
+                raise NotImplementedError
+
+            chief_req = LiteHMRCChiefRequest.objects.create(
+                import_application=application,
+                case_reference=application.reference,
+                request_sent_datetime=timezone.now(),
             )
             serialize = get_serializer(application.process_type)
-            data = serialize(application.get_specific_model(), str(chief_ref.lite_hmrc_id))
+            data = serialize(application.get_specific_model(), str(chief_req.lite_hmrc_id))
+
+            # Django JSONField encodes python objects therefore data.json() can't be used
+            chief_req.request_data = data.dict()
+            chief_req.save()
+
             request_license(data)
 
     except Exception:
         capture_exception()
         next_task = Task.TaskType.CHIEF_ERROR
 
-        if chief_ref:
-            chief_ref.delete()
+        if chief_req:
+            chief_req.delete()
 
     Task.objects.create(process=application, task_type=next_task, previous=previous_task)
 
