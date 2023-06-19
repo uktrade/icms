d18ed1ce86e902bb0a766f932ba6bc58ea859d59
Matthew Holmes <matthew.holmes@digital.trade.gov.uk>
2023-06-19 13:37:22 +0100

    Remove django-recaptcha dependency.

--- config/settings/local.py
+++ config/settings/local.py
@@ -8,11 +8,6 @@ DATABASES = {
 ALLOWED_HOSTS = ["localhost", "web"]
 DEBUG = True
 
-# Google recaptcha. Using fake test keys on localhost
-RECAPTCHA_PUBLIC_KEY = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"  # /PS-IGNORE
-RECAPTCHA_PRIVATE_KEY = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
-SILENCED_SYSTEM_CHECKS.extend(["captcha.recaptcha_test_key_error"])
-
 # for https://github.com/uktrade/django-chunk-s3-av-upload-handlers
 AWS_REGION = "eu-west-2"
 AWS_ACCESS_KEY_ID = "test"
