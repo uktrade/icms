6f746f9d302367d43529490edd8defb81ebe5651
Michael Lockwood <michael.lockwood@digital.trade.gov.uk>
2023-07-17 15:34:15 +0100

    ICMSLST-2132 Data Migration Import Search User Permissions

--- data_migration/management/commands/config/post_migrate.py
+++ data_migration/management/commands/config/post_migrate.py
@@ -7,4 +7,5 @@ GROUPS_TO_ROLES = {
     "Home Office Case Officer": queries.home_office_user_roles,
     "NCA Case Officer": queries.nca_user_roles,
     "Constabulary Contact": queries.constabulary_user_roles,
+    "Import Search User": queries.import_search_user_roles,
 }
