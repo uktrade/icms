4bb576d55b3f5bd27b163cebe7c2389344baf50b
Matthew Holmes <matthew.holmes@digital.trade.gov.uk>
2023-10-12 10:27:02 +0100

    ICMSLST-1603 Show summary of app type instead of "Application Details".

--- web/tests/flow/test_models.py
+++ web/tests/flow/test_models.py
@@ -1,5 +1,6 @@
 import pytest
 
+from web.flow.models import ProcessTypes
 from web.models import Process
 
 
@@ -9,3 +10,28 @@ def test_downcast_unknown():
 
     with pytest.raises(NotImplementedError, match="Unknown process_type blaa"):
         p.get_specific_model()
+
+
+@pytest.mark.parametrize(
+    "process_type,expected_label",
+    [
+        (ProcessTypes.FA_DFL, "Firearms and Ammunition"),
+        (ProcessTypes.FA_OIL, "Firearms and Ammunition"),
+        (ProcessTypes.FA_SIL, "Firearms and Ammunition"),
+        (ProcessTypes.IRON_STEEL, "Iron & Steel"),
+        (ProcessTypes.TEXTILES, "Textiles"),
+        (ProcessTypes.WOOD, "Wood"),
+        (ProcessTypes.DEROGATIONS, "Sanctions Derogation"),
+        (ProcessTypes.SPS, "Prior Surveillance"),
+        (ProcessTypes.SANCTIONS, "Sanctions and Adhoc"),
+        (ProcessTypes.OPT, "Outward Processing Trade"),
+        (ProcessTypes.CFS, "CFS Application"),
+        (ProcessTypes.COM, "COM Application"),
+        (ProcessTypes.GMP, "GMP Application"),
+        ("Unknown PT", "Application Details"),
+    ],
+)
+def test_get_application_details_link(process_type, expected_label):
+    process = Process(process_type=process_type)
+
+    assert process.get_application_details_link() == expected_label
