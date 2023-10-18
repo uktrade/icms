f342689e0928ebde82ccaf9512cabc986c26a88d
Matthew Holmes <matthew.holmes@digital.trade.gov.uk>
2023-10-18 14:20:24 +0100

    ICMSLST-1591 Do not show archived case notes when there are none.

---
+++ web/static/web/js/pages/case-notes.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', function (event) {
+    const currentNotesLink = document.querySelector("#notes-current");
+    const archivedNotesLink = document.querySelector("#notes-archived");
+
+    const showCurrentNotes = UTILS.getShowElementFunc("#current-notes-list");
+    const showArchivedNotes = UTILS.getShowElementFunc("#deleted-notes-list");
+
+    currentNotesLink.addEventListener("click", function (event) {
+        event.preventDefault();
+        showCurrentNotes(true);
+        showArchivedNotes(false);
+        currentNotesLink.classList.add("current-tab");
+        archivedNotesLink.classList.remove("current-tab");
+    });
+
+    archivedNotesLink.addEventListener("click", function (event) {
+        if (parseInt(this.dataset.noteCount) === 0) {
+            return;
+        }
+
+        event.preventDefault();
+        showCurrentNotes(false);
+        showArchivedNotes(true);
+        archivedNotesLink.classList.add("current-tab");
+        currentNotesLink.classList.remove("current-tab");
+    });
+});
