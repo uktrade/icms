a12549e370498de11bde46390172477ad85dcb50
christopherpettinga <chrispettinga@gmail.com>
2024-02-05 17:32:06 +0000

    Digital PDF signing with the endesive library
    Alignment of V2 PDFs with their V1 counterparts
    
    Simplification of PDF generation logic
    
    PDF generation with playwright
    
    Needed to revert original PR as it deleted some commits from main which were made whilst the original branch was being worked on.

--- scripts/initial-setup.sh
+++ scripts/initial-setup.sh
@@ -2,12 +2,29 @@
 
 set -e
 
+echo "Installing swig (required by endesive)"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  which -s brew
+  if [[ $? != 0 ]] ; then
+      echo "Homebrew not installed, please install it first. This is required to install swig."
+      exit 1
+  fi
+    brew install swig
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  sudo apt-get install swig
+fi
+
+
+echo "Creating virtualenv"
 virtualenv --python=python3.11 .venv
 . .venv/bin/activate
 
 echo "Installing local requirements in a virtualenv"
 pip install -U pip
 pip install -r requirements-dev.txt
+
+echo "Installing playwright browsers"
+playwright install
 
 echo "Installing pre-commit hooks"
 pre-commit install
--- web/utils/pdf/generator.py
+++ web/utils/pdf/generator.py
@@ -2,9 +2,9 @@ import io
 from dataclasses import dataclass
 from typing import Any
 
-import weasyprint
 from django.conf import settings
 from django.template.loader import render_to_string
+from playwright.sync_api import sync_playwright
 
 from web.domains.case.types import DocumentPack, ImpOrExp
 from web.flow.models import ProcessTypes
@@ -25,11 +25,19 @@ class PdfGenBase:
         :param target: Optional target to write to
         :return: None if target is supplied else bytes
         """
+        document_html = self.get_document_html()
+        with sync_playwright() as playwright:
+            browser = playwright.chromium.launch(headless=True)
+            context = browser.new_context()
+            page = context.new_page()
+            page.set_content(document_html)
+            pdf_data = page.pdf()
 
-        html_string = self.get_document_html()
-        html = weasyprint.HTML(string=html_string, base_url=get_icms_domain())
+            if target:
+                target.write(pdf_data)
+                return None
 
-        return html.write_pdf(target=target)
+            return pdf_data
 
     # TODO: Remove this when all the pdfs have been created
     # See web/domains/case/views/views_pdf.py for example
@@ -62,46 +70,19 @@ class PdfGenerator(PdfGenBase):
             DocumentTypes.COVER_LETTER_PRE_SIGN,
             DocumentTypes.COVER_LETTER_SIGNED,
         ]:
-            # TODO ICMSLST-697 Revisit when signing the - document may need different templates
             return "pdf/import/cover-letter.html"
-
-        if self.doc_type == DocumentTypes.LICENCE_PREVIEW:
+        else:
             match self.application.process_type:
                 case ProcessTypes.FA_OIL:
-                    return "pdf/import/fa-oil-licence-preview.html"
+                    return "pdf/import/fa-oil-licence.html"
                 case ProcessTypes.FA_DFL:
-                    return "pdf/import/fa-dfl-licence-preview.html"
+                    return "pdf/import/fa-dfl-licence.html"
                 case ProcessTypes.FA_SIL:
-                    return "pdf/import/fa-sil-licence-preview.html"
+                    return "pdf/import/fa-sil-licence.html"
                 case ProcessTypes.SANCTIONS:
                     return "pdf/import/sanctions-licence.html"
                 case ProcessTypes.WOOD:
                     return "pdf/import/wood-licence.html"
-                case _:
-                    return "web/domains/case/import/manage/preview-licence.html"
-
-        # TODO: ICMSLST-697 Revisit when signing the document (it may need its own context / template)
-        if self.doc_type in (DocumentTypes.LICENCE_PRE_SIGN, DocumentTypes.LICENCE_SIGNED):
-            match self.application.process_type:
-                case ProcessTypes.FA_OIL:
-                    return "pdf/import/fa-oil-licence-pre-sign.html"
-                case ProcessTypes.FA_DFL:
-                    return "pdf/import/fa-dfl-licence-pre-sign.html"
-                case ProcessTypes.FA_SIL:
-                    return "pdf/import/fa-sil-licence-pre-sign.html"
-                case ProcessTypes.SANCTIONS:
-                    return "pdf/import/sanctions-licence.html"
-                case ProcessTypes.WOOD:
-                    return "pdf/import/wood-licence.html"
-                case _:
-                    return "web/domains/case/import/manage/preview-licence.html"
-
-        if self.doc_type in (
-            DocumentTypes.CERTIFICATE_PREVIEW,
-            DocumentTypes.CERTIFICATE_PRE_SIGN,
-            DocumentTypes.CERTIFICATE_SIGNED,
-        ):
-            match self.application.process_type:
                 case ProcessTypes.CFS:
                     return "pdf/export/cfs-certificate.html"
                 case ProcessTypes.COM:
@@ -111,12 +92,9 @@ class PdfGenerator(PdfGenBase):
                 case _:
                     raise ValueError(f"Unsupported process type: {self.application.process_type}")
 
-        return super().get_template()
-
     def get_document_context(self) -> dict[str, Any]:
         """Return the document context"""
 
-        # This will be extended to return the correct context for a given process & doc type
         if self.doc_type in [
             DocumentTypes.COVER_LETTER_PREVIEW,
             DocumentTypes.COVER_LETTER_PRE_SIGN,
@@ -124,61 +102,63 @@ class PdfGenerator(PdfGenBase):
         ]:
             return utils.get_cover_letter_context(self.application, self.doc_type)
 
-        # TODO: ICMSLST-697 Revisit when signing the document (it may need its own context / template)
-        elif self.doc_type in [
-            DocumentTypes.LICENCE_PREVIEW,
-            DocumentTypes.LICENCE_PRE_SIGN,
-            DocumentTypes.LICENCE_SIGNED,
-        ]:
-            match self.application.process_type:
-                case ProcessTypes.FA_OIL:
-                    return utils.get_fa_oil_licence_context(
-                        self.application, self.doc_pack, self.doc_type
-                    )
-                case ProcessTypes.FA_DFL:
-                    return utils.get_fa_dfl_licence_context(
-                        self.application, self.doc_pack, self.doc_type
-                    )
-                case ProcessTypes.FA_SIL:
-                    return utils.get_fa_sil_licence_context(
-                        self.application, self.doc_pack, self.doc_type
-                    )
-                case ProcessTypes.SANCTIONS:
-                    return utils.get_sanctions_licence_context(
-                        self.application, self.doc_pack, self.doc_type
-                    )
-                case ProcessTypes.WOOD:
-                    return utils.get_wood_licence_context(
-                        self.application, self.doc_pack, self.doc_type
-                    )
-                case _:
-                    return utils.get_licence_context(self.application, self.doc_pack, self.doc_type)
+        if (
+            self.application.process_type
+            in [
+                ProcessTypes.CFS,
+                ProcessTypes.COM,
+                ProcessTypes.GMP,
+            ]
+            and not self.country
+        ):
+            raise ValueError("Country must be specified for export certificates")
 
-        elif self.doc_type in [
-            DocumentTypes.CERTIFICATE_PREVIEW,
-            DocumentTypes.CERTIFICATE_PRE_SIGN,
-            DocumentTypes.CERTIFICATE_SIGNED,
-        ]:
-            if not self.country:
-                raise ValueError("Country must be specified for export certificates")
+        match self.application.process_type:
+            case ProcessTypes.FA_OIL:
+                context = utils.get_fa_oil_licence_context(
+                    self.application, self.doc_pack, self.doc_type
+                )
+            case ProcessTypes.FA_DFL:
+                context = utils.get_fa_dfl_licence_context(
+                    self.application, self.doc_pack, self.doc_type
+                )
+            case ProcessTypes.FA_SIL:
+                context = utils.get_fa_sil_licence_context(
+                    self.application, self.doc_pack, self.doc_type
+                )
+            case ProcessTypes.SANCTIONS:
+                context = utils.get_sanctions_licence_context(
+                    self.application, self.doc_pack, self.doc_type
+                )
+            case ProcessTypes.WOOD:
+                context = utils.get_wood_licence_context(
+                    self.application, self.doc_pack, self.doc_type
+                )
+            case ProcessTypes.CFS:
+                context = utils.get_cfs_certificate_context(
+                    self.application,
+                    self.doc_pack,
+                    self.doc_type,
+                    self.country,  # type:ignore[arg-type]
+                )
+            case ProcessTypes.COM:
+                context = utils.get_com_certificate_context(
+                    self.application,
+                    self.doc_pack,
+                    self.doc_type,
+                    self.country,  # type:ignore[arg-type]
+                )
+            case ProcessTypes.GMP:
+                context = utils.get_gmp_certificate_context(
+                    self.application,
+                    self.doc_pack,
+                    self.doc_type,
+                    self.country,  # type:ignore[arg-type]
+                )
+            case _:
+                raise ValueError(f"Unsupported process type: {self.application.process_type}")
 
-            match self.application.process_type:
-                case ProcessTypes.CFS:
-                    return utils.get_cfs_certificate_context(
-                        self.application, self.doc_pack, self.doc_type, self.country
-                    )
-                case ProcessTypes.COM:
-                    return utils.get_com_certificate_context(
-                        self.application, self.doc_pack, self.doc_type, self.country
-                    )
-                case ProcessTypes.GMP:
-                    return utils.get_gmp_certificate_context(
-                        self.application, self.doc_pack, self.doc_type, self.country
-                    )
-                case _:
-                    raise ValueError(f"Unsupported process type: {self.application.process_type}")
-
-        return super().get_document_context()
+        return context
 
 
 @dataclass
