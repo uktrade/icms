730b3d669ff2ea2c8ec7eeec3fe6167caf6a8c36
Michael Lockwood <michael.lockwood@digital.trade.gov.uk>
2024-02-08 16:53:50 +0000

    ICMSLST-2508 Data Migration - Only include active countries

--- data_migration/queries/reference/__init__.py
+++ data_migration/queries/reference/__init__.py
@@ -1,3 +1,4 @@
 from .commodities import *  # NOQA
 from .countries import *  # NOQA
+from .data_counts import *  # NOQA
 from .fa_calibre import *  # NOQA
---
+++ data_migration/queries/reference/data_counts.py
@@ -0,0 +1,1 @@
+country_group_countries_count = "SELECT COUNT(*) FROM bpmmgr.country_group_countries cgc"
