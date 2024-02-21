1c835965d2a62be90895e60973a0d8f178850f35
Lawrence Goldstien <lawrence.goldstein@digital.trade.gov.uk>
2024-02-21 14:09:07 +0000

    ci: initial image build configuration

---
+++ .copilot/image_build_run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Exit early if something goes wrong
+set -e
+
+# Add commands below to run inside the container after all the other buildpacks have been applied
+export BUILD_STEP='True'
+export COPILOT_ENVIRONMENT_NAME='build'
+
+python manage.py collectstatic --noinput --traceback
+
+# TODO: Fix CommandError: An error occurred during rendering /workspace/web/templates/base.html: Missing connections string
+# python manage.py compress --force --engine jinja2
+
+# TODO: Needs sorting for PDF generation
+# su -c "python -m playwright install --with-deps chromium"
--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: scripts/entry.sh
+web: bash scripts/entry.sh
 celery_worker: celery --app=config.celery:app worker --loglevel=INFO -Q celery,mail
 celery_beat: celery --app=config.celery:app beat --loglevel=INFO
