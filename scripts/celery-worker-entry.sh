80fd17f7902fc037ddf52619bd49d00ea14e33e9
Matthew Holmes <matthew.holmes@digital.trade.gov.uk>
2024-02-21 16:40:11 +0000

    ICMSLST-2505 Add requirements for running Playwright in Cloud Foundry.
    Changes:
      - Add apt-buildpack to install playwright system dependencies.
      - Add entry script for celery worker to install playwright browser.
      - Install playwright browser in web entry script.
      - Scale celery_worker process to fix memory issues:
        - cf scale icms-dev -k 2G --process celery_worker
        - cf scale icms-dev -m 2G --process celery_worker
    
    The celery_worker disk space increase is to accommodate the extra install space
    required for chromium.
    
    The celery_worker RAM increase fixes an out of memory exception caused when
    signing the PDF.

---
+++ scripts/celery-worker-entry.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+if [ -n "${COPILOT_ENVIRONMENT_NAME}" ]; then
+    echo "Running in DBT Platform"
+    celery --app=config.celery:app worker --loglevel=INFO -Q celery,mail
+
+else
+    echo "Running in Cloud Foundry"
+    # In DBT platform the following will be done at the build stage
+    echo "Installing playwright chromium browser for celery worker."
+    playwright install chromium
+
+    celery --app=config.celery:app worker --loglevel=INFO -Q celery,mail
+fi
