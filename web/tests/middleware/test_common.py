ca6e708ba634575908faaae1e1e9edd927c99a2a
Matthew Holmes <matthew.holmes@digital.trade.gov.uk>
2024-02-26 09:22:53 +0000

    ICMSLST-2536 Add middleware to skip adding site attribute to request obj.
    The DBT Platform healthcheck uses the machine IP address as the host when
    calling the pingdom/ping.xml endpoint. This throws an exception as the IP
    address is not a valid site.

--- web/tests/middleware/test_common.py
+++ web/tests/middleware/test_common.py
@@ -1,6 +1,14 @@
-from unittest.mock import Mock
+from unittest.mock import Mock, create_autospec
 
-from web.middleware.common import ICMSMiddleware, ICMSMiddlewareContext
+import pytest
+from django.http import HttpRequest
+from django.urls import reverse
+
+from web.middleware.common import (
+    ICMSCurrentSiteMiddleware,
+    ICMSMiddleware,
+    ICMSMiddlewareContext,
+)
 from web.utils.lock_manager import LockManager
 
 
@@ -17,3 +25,26 @@ def test_request_icms():
 
     assert isinstance(request.icms, ICMSMiddlewareContext)
     assert isinstance(request.icms.lock_manager, LockManager)
+
+
+class TestICMSCurrentSiteMiddleware:
+    @pytest.fixture(autouse=True)
+    def setup(self, db):
+        self.request = create_autospec(HttpRequest)
+        self.request.get_host.return_value = "import-a-licence"
+
+    def test_request_site_not_added(self):
+        self.request.path = reverse("dbt-platform-health-check")
+
+        middleware = ICMSCurrentSiteMiddleware(Mock())
+        middleware(self.request)
+
+        assert not hasattr(self.request, "site")
+
+    def test_request_site_added(self):
+        self.request.path = reverse("workbasket")
+
+        middleware = ICMSCurrentSiteMiddleware(Mock())
+        middleware(self.request)
+
+        assert hasattr(self.request, "site")
