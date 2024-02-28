af1a555def3f3f3a9b17522337c3beaaf410ba56
christopherpettinga <chrispettinga@gmail.com>
2024-02-28 13:28:17 +0000

    ICMSLST-1349 - SupplementaryReportForm Restrict year range for Date Received to 6 years

--- web/forms/fields.py
+++ web/forms/fields.py
@@ -1,8 +1,10 @@
+import datetime as dt
 import re
 from typing import Any
 
 import phonenumber_field.formfields
 from django import forms
+from django.forms import Widget
 from phonenumber_field.widgets import PhoneNumberInternationalFallbackWidget
 
 from .utils import clean_postcode
@@ -69,3 +71,53 @@ JQUERY_DATE_FORMAT = "%d-%b-%Y"
 class JqueryDateField(forms.DateField):
     widget = DateInput(format=JQUERY_DATE_FORMAT)
     input_formats = [JQUERY_DATE_FORMAT]
+
+    def __init__(self, *args, year_select_range: int = 100, **kwargs) -> None:
+        self.year_select_range = year_select_range
+        super().__init__(*args, **kwargs)
+
+    def widget_attrs(self, widget: Widget) -> dict[str, Any]:
+        attrs = super().widget_attrs(widget)
+        # we want to pass this attribute to the widget so jQuery can use it
+        attrs["data-year-select-range"] = self.year_select_range
+        return attrs
+
+    def validate(self, value: dt.datetime) -> None:
+        """Check if the date is within the year_select_range (if it has been provided)."""
+        if self.year_select_range and value:
+            current_year = dt.date.today().year
+            if abs(value.year - current_year) > self.year_select_range:
+                raise forms.ValidationError(
+                    f"Date cannot be more than {self.year_select_range} years in the past/future."
+                )
+        return super().validate(value)
+
+
+class PastOnlyJqueryDateField(JqueryDateField):
+    """A jQuery datepicker field that only allows past dates."""
+
+    def widget_attrs(self, widget: Widget) -> dict[str, Any]:
+        attrs = super().widget_attrs(widget)
+        attrs["data-past-only"] = "yes"
+        return attrs
+
+    def validate(self, value: dt.datetime) -> None:
+        current_year = dt.date.today().year
+        if value.year > current_year:
+            raise forms.ValidationError("Date cannot be in the future.")
+        return super().validate(value)
+
+
+class FutureOnlyJqueryDateField(JqueryDateField):
+    """A jQuery datepicker field that only allows future dates."""
+
+    def widget_attrs(self, widget: Widget) -> dict[str, Any]:
+        attrs = super().widget_attrs(widget)
+        attrs["data-future-only"] = "yes"
+        return attrs
+
+    def validate(self, value: dt.datetime) -> None:
+        current_year = dt.date.today().year
+        if value.year < current_year:
+            raise forms.ValidationError("Date cannot be in the past.")
+        return super().validate(value)
