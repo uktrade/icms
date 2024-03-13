ec3cf952e8336c1ea574192c2db5d4f21948eda0
Lawrence Goldstien <lawrence.goldstein@digital.trade.gov.uk>
2024-03-13 08:23:32 +0000

    DPM-159 - add build configuration for DBT platform

---
+++ .copilot/image_build_run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Exit early if something goes wrong
+set -e
+
+# Add commands below to run inside the container after all the other buildpacks have been applied
+export BUILD_STEP='True'
+export COPILOT_ENVIRONMENT_NAME='build'
+
+echo "Running python manage.py collectstatic --noinput --traceback"
+python manage.py collectstatic --noinput --traceback
+
+echo "Running compress python manage.py compress --force --engine jinja2"
+python manage.py compress --force --engine jinja2
+
+echo 'Running PLAYWRIGHT_BROWSERS_PATH="/workspace/playwright-deps" python -m playwright install'
+PLAYWRIGHT_BROWSERS_PATH="/workspace/playwright-deps" python -m playwright install
