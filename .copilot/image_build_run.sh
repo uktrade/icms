60bea07d41aa06107633a48a330e534a54334f97
Lawrence Goldstien <lawrence.goldstein@digital.trade.gov.uk>
2024-03-18 17:07:25 +0000

    DPM-204 - Bump builder and use playwright buildpack

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -12,6 +12,3 @@ python manage.py collectstatic --noinput --traceback
 
 echo "Running compress python manage.py compress --force --engine jinja2"
 python manage.py compress --force --engine jinja2
-
-echo 'Running PLAYWRIGHT_BROWSERS_PATH="/workspace/playwright-deps" python -m playwright install'
-PLAYWRIGHT_BROWSERS_PATH="/workspace/playwright-deps" python -m playwright install
