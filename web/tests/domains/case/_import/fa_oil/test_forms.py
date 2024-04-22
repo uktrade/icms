9e2e84d6ee83d4aeda939bfda071ca88c93684d6
christopherpettinga <chrispettinga@gmail.com>
2024-04-22 13:39:52 +0100

    ICMSLST-1260 - Firearms OIL - Application - Certificate Type field does not need drop-down list.

--- web/tests/domains/case/_import/fa_oil/test_forms.py
+++ web/tests/domains/case/_import/fa_oil/test_forms.py
@@ -1,5 +1,7 @@
 import pytest
 
+from web.domains.case._import.fa.forms import UserImportCertificateForm
+from web.domains.case._import.fa.models import UserImportCertificate
 from web.domains.case._import.fa_oil.forms import EditFaOILForm
 from web.models import Country
 from web.models.shared import FirearmCommodity
@@ -51,3 +53,13 @@ class TestOILForm(AuthTestCase):
 
         assert form.fields["consignment_country"].queryset.count() == 1
         assert form.fields["consignment_country"].queryset.get() == self.all_countries_object
+
+
+def test_user_import_certificate_form(fa_oil_app_in_progress):
+    form = UserImportCertificateForm(application=fa_oil_app_in_progress)
+    assert len(form.fields["certificate_type"].choices) == 1
+    assert (
+        form.fields["certificate_type"].initial
+        == UserImportCertificate.CertificateType.registered.value
+    )
+    assert form.fields["certificate_type"].disabled
