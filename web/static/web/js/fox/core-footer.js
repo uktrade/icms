7d70995c66f375f16b1c53d47c1a80bfc5cc207b
Marcus Patino Pan <marcus.patinopan@trade.gov.uk>
2024-06-17 13:32:01 +0100

    ICMSLST-2663 - Fix datepicker today button

--- web/static/web/js/fox/core-footer.js
+++ web/static/web/js/fox/core-footer.js
@@ -60,6 +60,27 @@ var FOXjs = {
       maxWidth: 250
     });
 
+    // Fixes issue with Today button https://stackoverflow.com/questions/1073410/today-button-in-jquery-datepicker-doesnt-work
+    $.datepicker._gotoToday = function (id){
+      var target = $(id);
+      var inst = this._getInst(target[0]);
+      if (this._get(inst, 'gotoCurrent') && inst.currentDay) {
+        inst.selectedDay = inst.currentDay;
+        inst.drawMonth = inst.selectedMonth = inst.currentMonth;
+        inst.drawYear = inst.selectedYear = inst.currentYear;
+      } else {
+        var date = new Date();
+        inst.selectedDay = date.getDate();
+        inst.drawMonth = inst.selectedMonth = date.getMonth();
+        inst.drawYear = inst.selectedYear = date.getFullYear();
+        // the below two lines are new
+        this._setDateDatepicker(target, date);
+        this._selectDate(id, this._getDateDatepicker(target));
+      }
+      this._notifyChange(inst);
+      this._adjustDate(target);
+    }
+
     // Enable hints on focus of elements with hint icons, rather than requiring hovering the icon
     $("[data-hint-id]").on({
       focus: function() {
