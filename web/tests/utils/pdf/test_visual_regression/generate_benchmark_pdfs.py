4a36b481d8dddab9a97f265e7266b855dc9ea911
christopherpettinga <chrispettinga@gmail.com>
2024-07-03 15:45:49 +0100

    removing old stuff

--- web/tests/utils/pdf/test_visual_regression/generate_benchmark_pdfs.py
+++ web/tests/utils/pdf/test_visual_regression/generate_benchmark_pdfs.py
@@ -1,7 +1,7 @@
 """These tests generate benchmark PDFs for visual regression testing.
 
-They are not really tests and as such are not ran as part of the test suite. The reason they are tests is that they
-depend on pytest fixtures to populate the data needed to produce the PDFs."""
+They are not really tests and as such are not ran as part of the test suite. The reason they are written as tests is
+because they depend on pytest fixtures to populate the data needed to produce the PDFs."""
 
 import datetime as dt
 import json
