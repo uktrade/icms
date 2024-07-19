7659a801007af385b169bcbf29dc81158a543dea
Matthew Holmes <matthew.holmes@digital.trade.gov.uk>
2024-07-19 17:08:23 +0100

    ICMSLST-2624 Display case email attachments correctly for all app types.

--- web/tests/domains/case/views/test_views_documents.py
+++ web/tests/domains/case/views/test_views_documents.py
@@ -5,6 +5,7 @@ from unittest import mock
 import pytest
 from django.contrib.auth.models import AnonymousUser
 from django.http import QueryDict
+from django.urls import reverse
 from pytest_django.asserts import assertContains
 
 from web.domains.case.services import document_pack
@@ -13,6 +14,7 @@ from web.models import (
     CaseEmailDownloadLink,
     Constabulary,
     ConstabularyLicenceDownloadLink,
+    SanctionEmail,
 )
 from web.tests.helpers import CaseURLS
 
@@ -291,49 +293,7 @@ class TestRegenerateDFLCaseDocumentsDownloadLinkView:
         mocked_capture_exception.assert_not_called()
 
 
-def generate_case_email_download_link(client, submitted_app, case_type):
-    app = submitted_app
-    client.post(CaseURLS.take_ownership(app.pk, case_type))
-    app.refresh_from_db()
-    response = client.post(CaseURLS.create_case_emails(app.pk, case_type))
-    app.refresh_from_db()
-    case_email: CaseEmail = app.case_emails.last()
-    client.post(
-        response.url,
-        data={
-            "to": case_email.to,
-            "cc_address_list": [],
-            "attachments": case_email.attachments.values_list("pk", flat=True),
-            "subject": case_email.subject,
-            "body": case_email.body,
-            "send": "Send",
-        },
-    )
-    app.refresh_from_db()
-    return CaseEmailDownloadLink.objects.get(case_email=case_email)
-
-
-@pytest.fixture
-def beis_case_email_link(ilb_admin_client, gmp_app_submitted):
-    return generate_case_email_download_link(ilb_admin_client, gmp_app_submitted, "export")
-
-
-class TestDownloadBEISEmailDocumentsFormView:
-    @pytest.fixture(autouse=True)
-    def setup(self, cw_client, beis_case_email_link):
-        self.client = cw_client
-        self.link = beis_case_email_link
-        self.valid_url = CaseURLS.download_case_email_documents(self.link.code)
-        qd = QueryDict(mutable=True)
-        qd.update(
-            {
-                "email": self.link.email,
-                "check_code": self.link.check_code,
-            }
-        )
-        self.valid_query_string = qd.urlencode()
-        self.invalid_url = CaseURLS.download_case_email_documents(str(uuid.uuid4()))
-
+class CommonDownloadCaseEmailDocumentsFormViewTests:
     def test_get_valid_link(self):
         response = self.client.get(self.valid_url)
         assert response.status_code == HTTPStatus.OK
@@ -358,33 +318,6 @@ class TestDownloadBEISEmailDocumentsFormView:
 
         context = response.context
         assert context["form"].initial == {"email": None, "check_code": None}
-
-    @mock.patch("web.domains.case.views.views_documents.create_presigned_url")
-    def test_post_valid_link(self, mocked_create_presigned_url):
-        # to test the failure_count gets reset
-        self.link.failure_count = 2
-        self.link.save()
-
-        data = {
-            "email": self.link.email,
-            "check_code": self.link.check_code,
-        }
-
-        response = self.client.post(self.valid_url, data=data)
-        assert response.status_code == HTTPStatus.OK
-
-        self.link.refresh_from_db()
-        assert self.link.failure_count == 0
-
-        context = response.context
-
-        assert context["exporter"]["name"] == "Test Exporter 1"
-        assert len(context["documents"]) == 3
-        assert [doc["name"] for doc in context["documents"]] == [
-            "ISO 17065",
-            "ISO 17021",
-            "ISO 22716",
-        ]
 
     def test_post_valid_link_expires_after_reaching_failure_limit(self):
         assert self.link.failure_count == 0
@@ -423,22 +356,14 @@ class TestDownloadBEISEmailDocumentsFormView:
         assertContains(response, "Regenerate Link")
 
 
-class TestRegenerateBEISCaseEmailDownloadLinkView:
-    @pytest.fixture(autouse=True)
-    def setup(self, cw_client, beis_case_email_link):
-        self.client = cw_client
-        self.link = beis_case_email_link
-
-        self.valid_url = CaseURLS.regenerate_case_email_documents_link(self.link.code)
-        self.invalid_url = CaseURLS.regenerate_case_email_documents_link(str(uuid.uuid4()))
-
+class CommonRegenerateCaseEmailDocumentsDownloadLinkViewTests:
     def test_post_only(self):
         response = self.client.get(self.valid_url)
         assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
 
     @mock.patch("web.domains.case.views.views_documents.send_case_email")
     @mock.patch("web.domains.case.views.views_documents.capture_exception")
-    def test_post_valid_link_arse(self, mocked_capture_exception, mocked_send_case_email):
+    def test_post_valid_link(self, mocked_capture_exception, mocked_send_case_email):
         assert not self.link.expired
         response = self.client.post(self.valid_url, follow=True)
         assert response.status_code == HTTPStatus.OK
@@ -484,3 +409,386 @@ class TestRegenerateBEISCaseEmailDownloadLinkView:
         mocked_send_case_email.assert_not_called()
         # And no sentry was raised as an ObjectDoesNotExist was raised
         mocked_capture_exception.assert_not_called()
+
+
+def generate_case_email_download_link(client, submitted_app, case_type, send_to=None):
+    app = submitted_app
+    client.post(CaseURLS.take_ownership(app.pk, case_type))
+    app.refresh_from_db()
+    response = client.post(CaseURLS.create_case_emails(app.pk, case_type), follow=True)
+    app.refresh_from_db()
+
+    case_email: CaseEmail = app.case_emails.last()
+
+    case_email_post_data = {
+        "to": send_to or case_email.to,
+        "cc_address_list": [],
+        # Include all the available attachments in the form.
+        "attachments": list(response.context["file_metadata"].keys()),
+        "subject": case_email.subject,
+        "body": case_email.body,
+        "send": "true",
+    }
+
+    edit_case_email_url = reverse(
+        "case:edit-case-email",
+        kwargs={
+            "application_pk": app.pk,
+            "case_email_pk": case_email.pk,
+            "case_type": case_type,
+        },
+    )
+
+    client.post(edit_case_email_url, data=case_email_post_data)
+    app.refresh_from_db()
+
+    return CaseEmailDownloadLink.objects.get(case_email=case_email)
+
+
+@pytest.fixture
+def beis_case_email_link(ilb_admin_client, gmp_app_submitted):
+    return generate_case_email_download_link(ilb_admin_client, gmp_app_submitted, "export")
+
+
+class TestGMPDownloadCaseEmailDocumentsFormView(CommonDownloadCaseEmailDocumentsFormViewTests):
+    @pytest.fixture(autouse=True)
+    def setup(self, cw_client, beis_case_email_link):
+        self.client = cw_client
+        self.link = beis_case_email_link
+        self.valid_url = CaseURLS.download_case_email_documents(self.link.code)
+        qd = QueryDict(mutable=True)
+        qd.update(
+            {
+                "email": self.link.email,
+                "check_code": self.link.check_code,
+            }
+        )
+        self.valid_query_string = qd.urlencode()
+        self.invalid_url = CaseURLS.download_case_email_documents(str(uuid.uuid4()))
+
+    @mock.patch("web.domains.case.views.views_documents.create_presigned_url")
+    def test_post_valid_link(self, mocked_create_presigned_url):
+        # to test the failure_count gets reset
+        self.link.failure_count = 2
+        self.link.save()
+
+        data = {
+            "email": self.link.email,
+            "check_code": self.link.check_code,
+        }
+
+        response = self.client.post(self.valid_url, data=data)
+        assert response.status_code == HTTPStatus.OK
+
+        self.link.refresh_from_db()
+        assert self.link.failure_count == 0
+
+        context = response.context
+
+        assert context["exporter"]["name"] == "Test Exporter 1"
+        assert len(context["documents"]) == 3
+
+        file_metadata = context["file_metadata"]
+        documents = context["documents"]
+        assert len(file_metadata.keys()) == 3
+        assert len(documents) == 3
+
+        assert sorted([md["file_type"] for pk, md in file_metadata.items()]) == sorted(
+            [
+                "ISO 17065",
+                "ISO 17021",
+                "ISO 22716",
+            ]
+        )
+
+
+class TestGMPRegenerateCaseEmailDocumentsDownloadLinkView(
+    CommonRegenerateCaseEmailDocumentsDownloadLinkViewTests
+):
+    @pytest.fixture(autouse=True)
+    def setup(self, cw_client, beis_case_email_link):
+        self.client = cw_client
+        self.link = beis_case_email_link
+
+        self.valid_url = CaseURLS.regenerate_case_email_documents_link(self.link.code)
+        self.invalid_url = CaseURLS.regenerate_case_email_documents_link(str(uuid.uuid4()))
+
+
+@pytest.fixture
+def sanctions_case_email_link(ilb_admin_client, sanctions_app_submitted):
+    send_to = SanctionEmail.objects.filter(is_active=True).first().email
+    return generate_case_email_download_link(
+        ilb_admin_client, sanctions_app_submitted, "import", send_to
+    )
+
+
+class TestSanctionsDownloadCaseEmailDocumentsFormView(
+    CommonDownloadCaseEmailDocumentsFormViewTests
+):
+    @pytest.fixture(autouse=True)
+    def setup(self, cw_client, sanctions_case_email_link):
+        self.client = cw_client
+        self.link = sanctions_case_email_link
+        self.valid_url = CaseURLS.download_case_email_documents(self.link.code)
+        qd = QueryDict(mutable=True)
+        qd.update(
+            {
+                "email": self.link.email,
+                "check_code": self.link.check_code,
+            }
+        )
+        self.valid_query_string = qd.urlencode()
+        self.invalid_url = CaseURLS.download_case_email_documents(str(uuid.uuid4()))
+
+    @mock.patch("web.domains.case.views.views_documents.create_presigned_url")
+    def test_post_valid_link(self, mocked_create_presigned_url):
+        # to test the failure_count gets reset
+        self.link.failure_count = 2
+        self.link.save()
+
+        data = {
+            "email": self.link.email,
+            "check_code": self.link.check_code,
+        }
+
+        response = self.client.post(self.valid_url, data=data)
+        assert response.status_code == HTTPStatus.OK
+
+        self.link.refresh_from_db()
+        assert self.link.failure_count == 0
+
+        context = response.context
+
+        assert context["importer"]["name"] == "Test Importer 1"
+        assert len(context["documents"]) == 1
+
+        file_metadata = context["file_metadata"]
+        documents = context["documents"]
+
+        assert len(file_metadata.keys()) == 1
+        assert len(documents) == 1
+
+        file = documents[0]["file"]
+        assert file_metadata[file.id]["file_type"] == "Supporting Documents"
+
+
+class TestSanctionsRegenerateCaseEmailDocumentsDownloadLinkView(
+    CommonRegenerateCaseEmailDocumentsDownloadLinkViewTests
+):
+    @pytest.fixture(autouse=True)
+    def setup(self, cw_client, sanctions_case_email_link):
+        self.client = cw_client
+        self.link = sanctions_case_email_link
+
+        self.valid_url = CaseURLS.regenerate_case_email_documents_link(self.link.code)
+        self.invalid_url = CaseURLS.regenerate_case_email_documents_link(str(uuid.uuid4()))
+
+
+@pytest.fixture
+def fa_sil_case_email_link(
+    ilb_admin_client, fa_sil_app_submitted, section5_authority, firearms_authority
+):
+    # Add authorities to app
+    fa_sil_app_submitted.verified_section5.add(section5_authority)
+    fa_sil_app_submitted.verified_certificates.add(firearms_authority)
+
+    send_to = SanctionEmail.objects.filter(is_active=True).first().email
+    return generate_case_email_download_link(
+        ilb_admin_client, fa_sil_app_submitted, "import", send_to
+    )
+
+
+class TestFASILDownloadCaseEmailDocumentsFormView(CommonDownloadCaseEmailDocumentsFormViewTests):
+    @pytest.fixture(autouse=True)
+    def setup(self, cw_client, fa_sil_case_email_link):
+        self.client = cw_client
+        self.link = fa_sil_case_email_link
+        self.valid_url = CaseURLS.download_case_email_documents(self.link.code)
+        qd = QueryDict(mutable=True)
+        qd.update(
+            {
+                "email": self.link.email,
+                "check_code": self.link.check_code,
+            }
+        )
+        self.valid_query_string = qd.urlencode()
+        self.invalid_url = CaseURLS.download_case_email_documents(str(uuid.uuid4()))
+
+    @mock.patch("web.domains.case.views.views_documents.create_presigned_url")
+    def test_post_valid_link(self, mocked_create_presigned_url):
+        # to test the failure_count gets reset
+        self.link.failure_count = 2
+        self.link.save()
+
+        data = {
+            "email": self.link.email,
+            "check_code": self.link.check_code,
+        }
+
+        response = self.client.post(self.valid_url, data=data)
+        assert response.status_code == HTTPStatus.OK
+
+        self.link.refresh_from_db()
+        assert self.link.failure_count == 0
+
+        context = response.context
+
+        assert context["importer"]["name"] == "Test Importer 1"
+
+        file_metadata = context["file_metadata"]
+        documents = context["documents"]
+
+        assert len(file_metadata.keys()) == 6
+        assert len(documents) == 6
+
+        doc_pks = [doc["file"].pk for doc in documents]
+        assert sorted(doc_pks) == sorted(file_metadata.keys())
+
+
+class TestFASILRegenerateCaseEmailDocumentsDownloadLinkView(
+    CommonRegenerateCaseEmailDocumentsDownloadLinkViewTests
+):
+    @pytest.fixture(autouse=True)
+    def setup(self, cw_client, fa_sil_case_email_link):
+        self.client = cw_client
+        self.link = fa_sil_case_email_link
+
+        self.valid_url = CaseURLS.regenerate_case_email_documents_link(self.link.code)
+        self.invalid_url = CaseURLS.regenerate_case_email_documents_link(str(uuid.uuid4()))
+
+
+@pytest.fixture
+def fa_oil_case_email_link(ilb_admin_client, fa_oil_app_submitted, firearms_authority):
+    # Add authorities to app
+    fa_oil_app_submitted.verified_certificates.add(firearms_authority)
+
+    send_to = SanctionEmail.objects.filter(is_active=True).first().email
+    return generate_case_email_download_link(
+        ilb_admin_client, fa_oil_app_submitted, "import", send_to
+    )
+
+
+class TestFAOILDownloadCaseEmailDocumentsFormView(CommonDownloadCaseEmailDocumentsFormViewTests):
+    @pytest.fixture(autouse=True)
+    def setup(self, cw_client, fa_oil_case_email_link):
+        self.client = cw_client
+        self.link = fa_oil_case_email_link
+        self.valid_url = CaseURLS.download_case_email_documents(self.link.code)
+        qd = QueryDict(mutable=True)
+        qd.update(
+            {
+                "email": self.link.email,
+                "check_code": self.link.check_code,
+            }
+        )
+        self.valid_query_string = qd.urlencode()
+        self.invalid_url = CaseURLS.download_case_email_documents(str(uuid.uuid4()))
+
+    @mock.patch("web.domains.case.views.views_documents.create_presigned_url")
+    def test_post_valid_link(self, mocked_create_presigned_url):
+        # to test the failure_count gets reset
+        self.link.failure_count = 2
+        self.link.save()
+
+        data = {
+            "email": self.link.email,
+            "check_code": self.link.check_code,
+        }
+
+        response = self.client.post(self.valid_url, data=data)
+        assert response.status_code == HTTPStatus.OK
+
+        self.link.refresh_from_db()
+        assert self.link.failure_count == 0
+
+        context = response.context
+
+        assert context["importer"]["name"] == "Test Importer 1"
+
+        file_metadata = context["file_metadata"]
+        documents = context["documents"]
+
+        assert len(file_metadata.keys()) == 3
+        assert len(documents) == 3
+
+        doc_pks = [doc["file"].pk for doc in documents]
+        assert sorted(doc_pks) == sorted(file_metadata.keys())
+
+
+class TestFAOILRegenerateCaseEmailDocumentsDownloadLinkView(
+    CommonRegenerateCaseEmailDocumentsDownloadLinkViewTests
+):
+    @pytest.fixture(autouse=True)
+    def setup(self, cw_client, fa_oil_case_email_link):
+        self.client = cw_client
+        self.link = fa_oil_case_email_link
+
+        self.valid_url = CaseURLS.regenerate_case_email_documents_link(self.link.code)
+        self.invalid_url = CaseURLS.regenerate_case_email_documents_link(str(uuid.uuid4()))
+
+
+@pytest.fixture
+def fa_dfl_case_email_link(ilb_admin_client, fa_dfl_app_submitted, firearms_authority):
+    send_to = SanctionEmail.objects.filter(is_active=True).first().email
+    return generate_case_email_download_link(
+        ilb_admin_client, fa_dfl_app_submitted, "import", send_to
+    )
+
+
+class TestFADFLDownloadCaseEmailDocumentsFormView(CommonDownloadCaseEmailDocumentsFormViewTests):
+    @pytest.fixture(autouse=True)
+    def setup(self, cw_client, fa_dfl_case_email_link):
+        self.client = cw_client
+        self.link = fa_dfl_case_email_link
+        self.valid_url = CaseURLS.download_case_email_documents(self.link.code)
+        qd = QueryDict(mutable=True)
+        qd.update(
+            {
+                "email": self.link.email,
+                "check_code": self.link.check_code,
+            }
+        )
+        self.valid_query_string = qd.urlencode()
+        self.invalid_url = CaseURLS.download_case_email_documents(str(uuid.uuid4()))
+
+    @mock.patch("web.domains.case.views.views_documents.create_presigned_url")
+    def test_post_valid_link(self, mocked_create_presigned_url):
+        # to test the failure_count gets reset
+        self.link.failure_count = 2
+        self.link.save()
+
+        data = {
+            "email": self.link.email,
+            "check_code": self.link.check_code,
+        }
+
+        response = self.client.post(self.valid_url, data=data)
+        assert response.status_code == HTTPStatus.OK
+
+        self.link.refresh_from_db()
+        assert self.link.failure_count == 0
+
+        context = response.context
+
+        assert context["importer"]["name"] == "Test Importer 1"
+
+        file_metadata = context["file_metadata"]
+        documents = context["documents"]
+
+        assert len(file_metadata.keys()) == 1
+        assert len(documents) == 1
+
+        doc_pks = [doc["file"].pk for doc in documents]
+        assert sorted(doc_pks) == sorted(file_metadata.keys())
+
+
+class TestFADFLRegenerateCaseEmailDocumentsDownloadLinkView(
+    CommonRegenerateCaseEmailDocumentsDownloadLinkViewTests
+):
+    @pytest.fixture(autouse=True)
+    def setup(self, cw_client, fa_dfl_case_email_link):
+        self.client = cw_client
+        self.link = fa_dfl_case_email_link
+
+        self.valid_url = CaseURLS.regenerate_case_email_documents_link(self.link.code)
+        self.invalid_url = CaseURLS.regenerate_case_email_documents_link(str(uuid.uuid4()))
