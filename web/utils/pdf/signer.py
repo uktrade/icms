0aefa626da8e5c2e5115598c32d552f647b7a118
Christopher Pettinga <chris.pettinga@digital.trade.gov.uk>
2024-07-25 16:15:10 +0100

    ICMSLST-2745 DFL Licence PDF document Signature Placement

--- web/utils/pdf/signer.py
+++ web/utils/pdf/signer.py
@@ -58,9 +58,9 @@ def get_signature_coordinates(
             # PyMuPDF uses the top left corner as 0,0 and endesive uses the bottom left corner as 0,0
             page_size = page.bound()
             signature_image_coordinates = (
-                # we add and subtract 5 for the top-left and top-bottom
-                # to give a little bit of padding around the signature image and ensure there is no overlap
-                signature_footer_text_rect.x0 - 5,
+                # we add to the top-right to give a little bit of padding around the signature image
+                # and ensure there is no overlap
+                signature_footer_text_rect.x0,
                 page_size.y1 - signature_footer_text_rect.y0,
                 signature_footer_text_rect.x1 + 5,
                 page_size.y1 - signature_header_text_rect.y1,
