4db34a7e344c584ef3a2ab8878da1b5ef7256c8c
Matthew Holmes <matthew.holmes@digital.trade.gov.uk>
2024-08-21 11:06:56 +0100

    ICMSLST-2920 Fix duplicate CFSProduct.product_name records.

--- data_migration/management/commands/extract_v1_xml.py
+++ data_migration/management/commands/extract_v1_xml.py
@@ -1,5 +1,9 @@
 from itertools import islice
 
+from django.db.models import Count
+
+from data_migration.models import CFSProduct, CFSProductTemplate
+
 from ._base import MigrationBaseCommand
 from .config.run_order import DATA_TYPE, DATA_TYPE_XML
 from .utils.format import format_name
@@ -20,6 +24,8 @@ class Command(MigrationBaseCommand):
         self._extract_xml_data("user", options["skip_user"])
         self._extract_xml_data("import_application", options["skip_ia"])
         self._extract_xml_data("export_application", options["skip_export"])
+        self.fix_duplicate_cfs_product_names()
+
         self._log_script_end()
 
     def _extract_xml_data(self, data_type: DATA_TYPE, skip: bool) -> None:
@@ -56,3 +62,40 @@ class Command(MigrationBaseCommand):
             self._log_time(count=count)
 
         self.log("XML extraction complete")
+
+    def fix_duplicate_cfs_product_names(self) -> None:
+        """Fix duplicate data that V2 will reject because it violates the db constraints."""
+
+        # Fix CFSProduct records
+        products_with_duplicates = (
+            CFSProduct.objects.values("schedule", "product_name")
+            .annotate(count=Count("product_name"))
+            .filter(count__gt=1)
+        )
+
+        for pd in products_with_duplicates:
+            products = CFSProduct.objects.filter(
+                schedule_id=pd["schedule"], product_name=pd["product_name"]
+            ).order_by("pk")
+
+            for idx, product in enumerate(products):
+                if idx != 0:
+                    product.product_name = f"{product.product_name} ({idx})"
+                    product.save()
+
+        # Fix CFSProductTemplate records
+        products_with_duplicates = (
+            CFSProductTemplate.objects.values("schedule", "product_name")
+            .annotate(count=Count("product_name"))
+            .filter(count__gt=1)
+        )
+
+        for pd in products_with_duplicates:
+            products = CFSProductTemplate.objects.filter(
+                schedule_id=pd["schedule"], product_name=pd["product_name"]
+            ).order_by("pk")
+
+            for idx, product in enumerate(products):
+                if idx != 0:
+                    product.product_name = f"{product.product_name} ({idx})"
+                    product.save()
