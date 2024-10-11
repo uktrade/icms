51f7ead29ffc0093cfbdf74976a5cdc362b3d135
Michael Lockwood <michael.lockwood@digital.trade.gov.uk>
2024-10-11 17:24:07 +0100

    ICMSLST-2979 Script to add attachments to BEIS emails

--- data_migration/management/commands/add_missing_constabulary_email_attachments.py
+++ data_migration/management/commands/add_missing_constabulary_email_attachments.py
@@ -8,7 +8,7 @@ from django.core.serializers.json import DjangoJSONEncoder
 from data_migration import models as dm
 from data_migration.management.commands.utils.db_processor import OracleDBProcessor
 from data_migration.utils.format import adjust_icms_v1_datetime
-from web.models import File
+from web.models import CaseEmail, File
 from web.utils import s3 as s3_web
 
 constabulary_email_files_query = """
@@ -34,6 +34,45 @@ WHERE fft_id = :file_target_id
 """
 
 
+beis_email_attachments_files = """
+SELECT
+  e.ca_id
+  , e.email_body "body"
+  , x.sent_datetime
+  , x.target_id
+  , x.select_flag
+FROM impmgr.xview_cert_app_beis_emails e
+INNER JOIN impmgr.certificate_app_details cad ON cad.id = e.cad_id
+CROSS JOIN XMLTABLE('
+  for $g1 in /*/CASE/BEIS_EMAILS/EMAIL_LIST/EMAIL | <null />
+  where /*/CASE/BEIS_EMAILS/EMAIL_LIST/EMAIL and $g1/STATUS/text()
+  return
+  for $g2 in $g1/SEND/SUPPORTING_DOCUMENT_LIST/SUPPORTING_DOCUMENT | <null />
+  where $g1/SEND/SUPPORTING_DOCUMENT_LIST/SUPPORTING_DOCUMENT and $g2/SELECT_FLAG[text()="true"]
+  return
+  <root>
+    <email_id>{$g1/EMAIL_ID/text()}</email_id>
+    <status>{$g1/STATUS/text()}</status>
+    <sent_datetime>{$g1/SEND/SENT_DATETIME/text()}</sent_datetime>
+    <target_id>{$g2/TARGET_ID/text()}</target_id>
+    <select_flag>{$g2/SELECT_FLAG/text()}</select_flag>
+  </root>
+  '
+  PASSING cad.xml_data
+  COLUMNS
+    email_id NUMBER PATH '/root/email_id/text()'
+  , status VARCHAR2(4000) PATH '/root/status/text()'
+  , sent_datetime VARCHAR2(4000) PATH '/root/sent_datetime/text()'
+  , target_id VARCHAR2(4000) PATH '/root/target_id/text()'
+  , select_flag VARCHAR2(4000) PATH '/root/select_flag/text()'
+) x
+WHERE e.status_control = 'C'
+AND x.email_id = e.email_id
+AND e.status <> ' DELETED'
+ORDER BY e.cad_id, e.email_id
+"""
+
+
 class Command(BaseCommand):
     def add_arguments(self, parser: argparse.ArgumentParser) -> None:
         parser.add_argument(
@@ -42,24 +81,62 @@ class Command(BaseCommand):
             action="store_true",
         )
 
-    def get_db(self) -> OracleDBProcessor:
+    def get_db(self, query_name: str) -> OracleDBProcessor:
         """Returns a DB processor that facilitates executing database queries on the v1 oracle database."""
-        return OracleDBProcessor(100, ["Constabulary Email Missing Attachments"], 100)
+        return OracleDBProcessor(100, [query_name], 100)
 
     def handle(self, *args: Any, **options: Any) -> None:
         self.dry_run = options["dry_run"]
         self.add_files_constabulary_email_attachments()
+
+    def add_beis_email_attachments(self) -> None:
+        """Finds the files attached to the BEIS emails V1 and links the attachments to CaseEmail in V2"""
+
+        sql = beis_email_attachments_files
+        db = self.get_db("BEIS Email Attachments")
+
+        with db.execute_query(sql, {}) as rows:
+            try:
+                while True:
+                    row = next(rows)
+                    self.add_attachements_to_beis_email(row)
+            except StopIteration:
+                pass
+
+    def add_attachments_to_beis_email(self, row: dict[str, Any]) -> None:
+        email_data = dict(
+            ca_id=row["CA_ID"],
+            body=row["BODY"],
+            status=row["STATUS"],
+            sent_datetime=adjust_icms_v1_datetime(row["SENT_DATETIME"]),
+            template_code=["CA_BEIS_EMAIL"],
+        )
+
+        dm_case_email = dm.CaseEmail.objects.get(**email_data)
+        case_email = CaseEmail.objects.get(pk=dm_case_email.pk)
+        app = case_email.export_applications.first().get_specific_model()
+
+        dm_file_pks = dm.File.objects.filter(target_id=row["TARGET_ID"]).values_list(
+            "pk", flat=True
+        )
+        files = app.supporting_documents.filter(pk__in=dm_file_pks)
+
+        if self.dry_run:
+            print(f"{app.reference} - Adding {files.count()} files to BEIS Email")
+            return
+
+        for file in files:
+            case_email.attachments.add(file)
 
     def add_files_constabulary_email_attachments(self) -> None:
         """Finds the constabulary emails with missing attachments in the data migration tables
         - Fetches the missing files from the V1 database
         - Uploads missing attachment files to S3
         - Adds the file data to V2 db
-        - Associates the file data with the relevant case email
         """
 
         sql = constabulary_email_files_query
-        db = self.get_db()
+        db = self.get_db("Missing Constabulary Email Attachments")
 
         qs = (
             dm.ConstabularyEmailAttachments.objects.filter(file_target__files__isnull=True)
