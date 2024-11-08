d7133e899ee1c4d58b7b80af6775b14b9b39dd76
Matthew Holmes <matthew.holmes@digital.trade.gov.uk>
2024-11-08 15:37:46 +0000

    ECIL-285 Refactor exporter/urls.py to define public & private lists.

--- web/domains/exporter/urls.py
+++ web/domains/exporter/urls.py
@@ -1,13 +1,12 @@
+from django.conf import settings
 from django.urls import path
 
 from . import views
 
-urlpatterns = [
-    path("", views.ExporterListAdminView.as_view(), name="exporter-list"),
+public_urls = [
     path("list/user/", views.ExporterListUserView.as_view(), name="user-exporter-list"),
     path("<int:pk>/", views.detail_exporter, name="exporter-view"),
     path("<int:pk>/edit/", views.edit_exporter, name="exporter-edit"),
-    path("create/", views.create_exporter, name="exporter-create"),
     path(
         "<int:org_pk>/user/<int:user_pk>/object_perms/",
         views.edit_user_exporter_permissions,
@@ -31,8 +30,19 @@ urlpatterns = [
         name="exporter-office-unarchive",
     ),
     # Exporter Agents
-    path("<int:exporter_pk>/agent/create/", views.create_agent, name="exporter-agent-create"),
     path("agent/<int:pk>/edit/", views.edit_agent, name="exporter-agent-edit"),
+]
+
+private_urls = [
+    path("", views.ExporterListAdminView.as_view(), name="exporter-list"),
+    path("create/", views.create_exporter, name="exporter-create"),
+    # Exporter Agents
+    path("<int:exporter_pk>/agent/create/", views.create_agent, name="exporter-agent-create"),
     path("agent/<int:pk>/archive/", views.archive_agent, name="exporter-agent-archive"),
     path("agent/<int:pk>/unarchive/", views.unarchive_agent, name="exporter-agent-unarchive"),
 ]
+
+if settings.INCLUDE_PRIVATE_URLS:
+    urlpatterns = public_urls + private_urls
+else:
+    urlpatterns = public_urls
