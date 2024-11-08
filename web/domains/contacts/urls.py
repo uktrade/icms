0d04f9ee30375a04b933aec51db5d2826c660687
Matthew Holmes <matthew.holmes@digital.trade.gov.uk>
2024-11-08 12:57:56 +0000

    ECIL-285 Refactor contacts/urls.py to define public & private lists.

--- web/domains/contacts/urls.py
+++ web/domains/contacts/urls.py
@@ -1,23 +1,15 @@
+from django.conf import settings
 from django.urls import include, path
 
 from . import views
 
 app_name = "contacts"
 
-urlpatterns = [
+public_urls = [
     path(
-        "<orgtype:org_type>/<int:org_pk>/",
-        include(
-            [
-                path("add/", views.add, name="add"),
-                path("<int:contact_pk>/delete/", views.delete, name="delete"),
-                path(
-                    "invite-contact/",
-                    views.InviteOrgContactView.as_view(),
-                    name="invite-org-contact",
-                ),
-            ]
-        ),
+        "<orgtype:org_type>/<int:org_pk>/invite-contact/",
+        views.InviteOrgContactView.as_view(),
+        name="invite-org-contact",
     ),
     path(
         "accept-org-invite/<uuid:code>/",
@@ -25,3 +17,20 @@ urlpatterns = [
         name="accept-org-invite",
     ),
 ]
+
+private_urls = [
+    path(
+        "<orgtype:org_type>/<int:org_pk>/",
+        include(
+            [
+                path("add/", views.add, name="add"),
+                path("<int:contact_pk>/delete/", views.delete, name="delete"),
+            ]
+        ),
+    ),
+]
+
+if settings.INCLUDE_PRIVATE_URLS:
+    urlpatterns = public_urls + private_urls
+else:
+    urlpatterns = public_urls
