c0cbe220e8354d9eb59cb751e46d3e442f2185bc
Matthew Holmes <matthew.holmes@digital.trade.gov.uk>
2024-11-08 16:17:42 +0000

    ECIL-285 Refactor legacy/urls.py to define public & private lists.

--- web/domains/case/_import/legacy/urls.py
+++ web/domains/case/_import/legacy/urls.py
@@ -1,31 +1,23 @@
+from django.conf import settings
 from django.urls import path
 
 from . import views
 
 app_name = "legacy"
 
-urlpatterns = [
+
+public_urls = [
     # Outward Processing Trade (OPT) legacy urls
     path(
         "opt/<int:application_pk>/document/<int:document_pk>/view/",
         views.opt_view_document,
         name="opt-view-document",
     ),
-    path(
-        "opt/<int:application_pk>/checklist/",
-        views.opt_manage_checklist,
-        name="opt-manage-checklist",
-    ),
     # Textiles (TEX) legacy urls
     path(
         "tex/<int:application_pk>/document/<int:document_pk>/view/",
         views.tex_view_document,
         name="tex-view-document",
-    ),
-    path(
-        "tex/<int:application_pk>/checklist/",
-        views.tex_manage_checklist,
-        name="tex-manage-checklist",
     ),
     # Prior Surveillance (SPS) legacy urls
     path(
@@ -39,3 +31,23 @@ urlpatterns = [
         name="sps-view-supporting-document",
     ),
 ]
+
+private_urls = [
+    # Outward Processing Trade (OPT) legacy urls
+    path(
+        "opt/<int:application_pk>/checklist/",
+        views.opt_manage_checklist,
+        name="opt-manage-checklist",
+    ),
+    # Textiles (TEX) legacy urls
+    path(
+        "tex/<int:application_pk>/checklist/",
+        views.tex_manage_checklist,
+        name="tex-manage-checklist",
+    ),
+]
+
+if settings.INCLUDE_PRIVATE_URLS:
+    urlpatterns = public_urls + private_urls
+else:
+    urlpatterns = public_urls
