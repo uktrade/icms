bccec819ad3d9d0c2549795af63e831cbf21611c
Matthew Holmes <matthew.holmes@digital.trade.gov.uk>
2024-11-11 09:56:18 +0000

    ECIL-285 Refactor case/urls.py to define public & private lists.

--- web/domains/case/urls.py
+++ web/domains/case/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.urls import include, path
 
 from web.types import DocumentTypes
@@ -20,7 +21,7 @@ from .views import (
 
 app_name = "case"
 
-search_urls = [
+public_search_urls = [
     path("<str:mode>/", views_search.search_cases, name="search"),
     path(
         "<str:mode>/results/",
@@ -34,33 +35,13 @@ search_urls = [
         name="search-download-spreadsheet",
     ),
     path(
-        "search-reassign-case-owner",
-        views_search.reassign_case_owner,
-        name="search-reassign-case-owner",
-    ),
-    path(
         "search-actions/<int:application_pk>/",
         include(
             [
                 path(
-                    "reopen-case",
-                    views_search.ReopenApplicationView.as_view(),
-                    name="search-reopen-case",
-                ),
-                path(
                     "request-variation",
                     views_search.RequestVariationUpdateView.as_view(),
                     name="search-request-variation",
-                ),
-                path(
-                    "open-variation",
-                    views_search.RequestVariationOpenRequestView.as_view(),
-                    name="search-open-variation",
-                ),
-                path(
-                    "search-revoke-case",
-                    views_search.RevokeCaseView.as_view(),
-                    name="search-revoke-case",
                 ),
                 path(
                     "copy-export-application",
@@ -77,7 +58,38 @@ search_urls = [
     ),
 ]
 
-note_urls = [
+private_search_urls = [
+    path(
+        "search-reassign-case-owner",
+        views_search.reassign_case_owner,
+        name="search-reassign-case-owner",
+    ),
+    path(
+        "search-actions/<int:application_pk>/",
+        include(
+            [
+                path(
+                    "reopen-case",
+                    views_search.ReopenApplicationView.as_view(),
+                    name="search-reopen-case",
+                ),
+                path(
+                    "open-variation",
+                    views_search.RequestVariationOpenRequestView.as_view(),
+                    name="search-open-variation",
+                ),
+                path(
+                    "search-revoke-case",
+                    views_search.RevokeCaseView.as_view(),
+                    name="search-revoke-case",
+                ),
+            ]
+        ),
+    ),
+]
+
+# All notes urls are private
+private_note_urls = [
     path("list/", views_note.list_notes, name="list-notes"),
     path("add/", views_note.add_note, name="add-note"),
     path(
@@ -117,7 +129,8 @@ note_urls = [
     ),
 ]
 
-admin_urls = [
+# All admin urls are private
+private_admin_urls = [
     path("manage/", views_misc.manage_case, name="manage"),
     path("take-ownership/", views_misc.take_ownership, name="take-ownership"),
     path("release-ownership/", views_misc.release_ownership, name="release-ownership"),
@@ -125,7 +138,8 @@ admin_urls = [
     path("manage-withdrawals/", views_misc.manage_withdrawals, name="manage-withdrawals"),
 ]
 
-applicant_urls = [
+# All applicant urls are public
+public_applicant_urls = [
     path("cancel/", views_misc.cancel_case, name="cancel"),
     path("withdraw/", views_misc.withdraw_case, name="withdraw-case"),
     path(
@@ -136,7 +150,22 @@ applicant_urls = [
     path("clear/", views_misc.ClearCaseFromWorkbasket.as_view(), name="clear"),
 ]
 
-variation_request_urls = [
+public_variation_request_urls = [
+    path(
+        "applicant/",
+        include(
+            [
+                path(
+                    "<int:variation_request_pk>/submit-update/",
+                    views_variation_request.VariationRequestRespondToUpdateRequestView.as_view(),
+                    name="variation-request-submit-update",
+                )
+            ]
+        ),
+    ),
+]
+
+private_variation_request_urls = [
     path(
         "admin/",
         include(
@@ -171,38 +200,19 @@ variation_request_urls = [
             ]
         ),
     ),
-    path(
-        "applicant/",
-        include(
-            [
-                path(
-                    "<int:variation_request_pk>/submit-update/",
-                    views_variation_request.VariationRequestRespondToUpdateRequestView.as_view(),
-                    name="variation-request-submit-update",
-                )
-            ]
-        ),
-    ),
 ]
 
-further_information_requests_urls = [
-    path("manage/", views_fir.manage_firs, name="manage-firs"),
+public_further_information_requests_urls = [
     path("list/", views_fir.list_firs, name="list-firs"),
-    path("add/", views_fir.add_fir, name="add-fir"),
     path(
         "<int:fir_pk>/",
         include(
             [
-                path("edit/", views_fir.edit_fir, name="edit-fir"),
                 path("respond/", views_fir.respond_fir, name="respond-fir"),
-                path("delete/", views_fir.delete_fir, name="delete-fir"),
-                path("withdraw/", views_fir.withdraw_fir, name="withdraw-fir"),
-                path("close/", views_fir.close_fir, name="close-fir"),
                 path(
                     "files/",
                     include(
                         [
-                            path("add/", views_fir.add_fir_file, name="add-fir-file"),
                             path(
                                 "response/add/",
                                 views_fir.add_fir_response_file,
@@ -216,11 +226,6 @@ further_information_requests_urls = [
                                             "view/",
                                             views_fir.view_fir_file,
                                             name="view-fir-file",
-                                        ),
-                                        path(
-                                            "delete/",
-                                            views_fir.delete_fir_file,
-                                            name="delete-fir-file",
                                         ),
                                         path(
                                             "response/delete/",
@@ -238,7 +243,57 @@ further_information_requests_urls = [
     ),
 ]
 
-update_requests_urls = [
+private_further_information_requests_urls = [
+    path("manage/", views_fir.manage_firs, name="manage-firs"),
+    path("add/", views_fir.add_fir, name="add-fir"),
+    path(
+        "<int:fir_pk>/",
+        include(
+            [
+                path("edit/", views_fir.edit_fir, name="edit-fir"),
+                path("delete/", views_fir.delete_fir, name="delete-fir"),
+                path("withdraw/", views_fir.withdraw_fir, name="withdraw-fir"),
+                path("close/", views_fir.close_fir, name="close-fir"),
+                path(
+                    "files/",
+                    include(
+                        [
+                            path("add/", views_fir.add_fir_file, name="add-fir-file"),
+                            path(
+                                "<int:file_pk>/",
+                                include(
+                                    [
+                                        path(
+                                            "delete/",
+                                            views_fir.delete_fir_file,
+                                            name="delete-fir-file",
+                                        ),
+                                    ]
+                                ),
+                            ),
+                        ]
+                    ),
+                ),
+            ]
+        ),
+    ),
+]
+
+public_update_requests_urls = [
+    path(
+        "<int:update_request_pk>/",
+        include(
+            [
+                path(
+                    "start/", views_update_request.start_update_request, name="start-update-request"
+                ),
+            ]
+        ),
+    ),
+    path("respond/", views_update_request.respond_update_request, name="respond-update-request"),
+]
+
+private_update_requests_urls = [
     path("list/", views_update_request.list_update_requests, name="list-update-requests"),
     path("add/", views_update_request.add_update_request, name="add-update-request"),
     path(
@@ -254,18 +309,13 @@ update_requests_urls = [
                 path(
                     "close/", views_update_request.close_update_request, name="close-update-request"
                 ),
-                # Applicant url
-                path(
-                    "start/", views_update_request.start_update_request, name="start-update-request"
-                ),
             ]
         ),
     ),
-    # Applicant url
-    path("respond/", views_update_request.respond_update_request, name="respond-update-request"),
 ]
 
-authorisation_urls = [
+# All authorisation urls are private
+private_authorisation_urls = [
     path("start/", views_misc.start_authorisation, name="start-authorisation"),
     path("cancel/", views_misc.cancel_authorisation, name="cancel-authorisation"),
     path("authorise-documents/", views_misc.authorise_documents, name="authorise-documents"),
@@ -273,7 +323,8 @@ authorisation_urls = [
     path("quick-issue/", views_misc.QuickIssueApplicationView.as_view(), name="quick-issue"),
 ]
 
-case_progress_urls = [
+# All case_progress urls are private
+private_case_progress_urls = [
     path(
         "check-document-generation/",
         views_misc.CheckCaseDocumentGenerationView.as_view(),
@@ -286,7 +337,8 @@ case_progress_urls = [
     ),
 ]
 
-email_urls = [
+# All email urls are private
+private_email_urls = [
     path("manage/", views_email.manage_case_emails, name="manage-case-emails"),
     path("create/", views_email.create_draft_case_email, name="create-case-email"),
     path(
@@ -305,7 +357,20 @@ email_urls = [
     ),
 ]
 
-pdf_urls = [
+public_pdf_urls = [
+    path(
+        "view-case-document/<int:object_pk>/<int:casedocumentreference_pk>",
+        views_pdf.view_case_document,
+        name="view-case-document",
+    ),
+    path(
+        "view-static-document/<int:file_pk>/",
+        views_pdf.view_static_document,
+        name="view-static-document",
+    ),
+]
+
+private_pdf_urls = [
     path(
         "licence/",
         include(
@@ -363,26 +428,15 @@ pdf_urls = [
             ]
         ),
     ),
-    path(
-        "view-case-document/<int:object_pk>/<int:casedocumentreference_pk>",
-        views_pdf.view_case_document,
-        name="view-case-document",
-    ),
-    path(
-        "view-static-document/<int:file_pk>/",
-        views_pdf.view_static_document,
-        name="view-static-document",
-    ),
 ]
 
-
-urlpatterns = [
+public_urls = [
     path(
         "<casetype:case_type>/",
         include(
             [
                 # search (import/export)
-                path("search/", include(search_urls)),
+                path("search/", include(public_search_urls)),
                 #
                 path(
                     "<int:application_pk>/",
@@ -392,32 +446,13 @@ urlpatterns = [
                             path("view/", views_view_case.view_case, name="view"),
                             #
                             # further information requests ((import/export/accessrequest))
-                            path("firs/", include(further_information_requests_urls)),
+                            path("firs/", include(public_further_information_requests_urls)),
                             #
                             # applicant case management
-                            path("applicant/", include(applicant_urls)),
-                            #
-                            # ILB Admin Case management (import/export)
-                            path("admin/", include(admin_urls)),
+                            path("applicant/", include(public_applicant_urls)),
                             #
                             # update requests
-                            path("update-requests/", include(update_requests_urls)),
-                            #
-                            # notes (import/export)
-                            path("notes/", include(note_urls)),
-                            #
-                            # misc stuff (import/export)
-                            path(
-                                "prepare-response/",
-                                views_prepare_response.prepare_response,
-                                name="prepare-response",
-                            ),
-                            #
-                            # Application Authorisation (import/export)
-                            path("authorisation/", include(authorisation_urls)),
-                            #
-                            # Progress urls (import/export)
-                            path("progress/", include(case_progress_urls)),
+                            path("update-requests/", include(public_update_requests_urls)),
                             #
                             # View Issued Case Documents (import/export)
                             path(
@@ -433,14 +468,11 @@ urlpatterns = [
                                 name="clear-issued-case-documents",
                             ),
                             #
-                            # Emails (import/export)
-                            path("emails/", include(email_urls)),
-                            #
                             # Variation request URLS:
-                            path("variation-request/", include(variation_request_urls)),
+                            path("variation-request/", include(public_variation_request_urls)),
                             #
                             # PDF generation URLs:
-                            path("pdf/", include(pdf_urls)),
+                            path("pdf/", include(public_pdf_urls)),
                             #
                             # Case history - two url confs to handle which base template to use.
                             path(
@@ -448,16 +480,6 @@ urlpatterns = [
                                 views_case_history.CaseHistoryView.as_view(),
                                 name="ilb-case-history",
                                 kwargs={"mode": "ilb"},
-                            ),
-                            path(
-                                "documents/<int:doc_pack_pk>/",
-                                views_constabulary.ConstabularyDocumentView.as_view(),
-                                name="constabulary-doc",
-                            ),
-                            path(
-                                "documents/<int:doc_pack_pk>/download/<int:cdr_pk>/",
-                                views_constabulary.ConstabularyDocumentDownloadView.as_view(),
-                                name="constabulary-doc-download",
                             ),
                             path(
                                 "applicant-case-history/",
@@ -471,6 +493,7 @@ urlpatterns = [
             ]
         ),
     ),
+    # TODO: Revisit in ECIL-295
     path(
         "download-dfl-case-documents/<uuid:code>/",
         views_documents.DownloadDFLCaseDocumentsFormView.as_view(),
@@ -492,3 +515,72 @@ urlpatterns = [
         name="regenerate-case-email-documents-link",
     ),
 ]
+
+private_urls = [
+    path(
+        "<casetype:case_type>/",
+        include(
+            [
+                # search (import/export)
+                path("search/", include(private_search_urls)),
+                #
+                path(
+                    "<int:application_pk>/",
+                    include(
+                        [
+                            #
+                            # further information requests ((import/export/accessrequest))
+                            path("firs/", include(private_further_information_requests_urls)),
+                            #
+                            # ILB Admin Case management (import/export)
+                            path("admin/", include(private_admin_urls)),
+                            #
+                            # update requests
+                            path("update-requests/", include(private_update_requests_urls)),
+                            #
+                            # notes (import/export)
+                            path("notes/", include(private_note_urls)),
+                            #
+                            # misc stuff (import/export)
+                            path(
+                                "prepare-response/",
+                                views_prepare_response.prepare_response,
+                                name="prepare-response",
+                            ),
+                            #
+                            # Application Authorisation (import/export)
+                            path("authorisation/", include(private_authorisation_urls)),
+                            #
+                            # Progress urls (import/export)
+                            path("progress/", include(private_case_progress_urls)),
+                            #
+                            # Emails (import/export)
+                            path("emails/", include(private_email_urls)),
+                            #
+                            # Variation request URLS:
+                            path("variation-request/", include(private_variation_request_urls)),
+                            #
+                            # PDF generation URLs:
+                            path("pdf/", include(private_pdf_urls)),
+                            path(
+                                "documents/<int:doc_pack_pk>/",
+                                views_constabulary.ConstabularyDocumentView.as_view(),
+                                name="constabulary-doc",
+                            ),
+                            path(
+                                "documents/<int:doc_pack_pk>/download/<int:cdr_pk>/",
+                                views_constabulary.ConstabularyDocumentDownloadView.as_view(),
+                                name="constabulary-doc-download",
+                            ),
+                        ]
+                    ),
+                ),
+            ]
+        ),
+    ),
+]
+
+if settings.INCLUDE_PRIVATE_URLS:
+    urlpatterns = public_urls + private_urls
+else:
+    urlpatterns = public_urls
