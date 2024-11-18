d64cc077c8a8195e0a0fdf5e45de888a7c8806a6
Christopher Pettinga <chris.pettinga@digital.trade.gov.uk>
2024-11-18 17:46:52 +0000

    ECIL-288 - Investigate why celery tasks get cancelled when we deploy ICMS

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -3,19 +3,5 @@
 echo "Running migrations"
 python manage.py migrate
 
-if [ -n "${COPILOT_ENVIRONMENT_NAME}" ]; then
-    echo "Running in DBT Platform"
-    opentelemetry-instrument gunicorn config.wsgi --config config/gunicorn.py
-else
-    echo "Running in Cloud Foundry"
-
-    # In DBT platform the following will be done at the build stage
-    echo "Installing playwright chromium browser"
-    playwright install chromium
-
-    echo "Collecting static files"
-    python manage.py collectstatic --noinput --traceback
-    python manage.py compress --engine jinja2
-
-    gunicorn config.wsgi --config config/gunicorn.py
-fi
+echo "Starting server"
+opentelemetry-instrument gunicorn config.wsgi --config config/gunicorn.py
