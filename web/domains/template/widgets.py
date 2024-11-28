eba02a2f01be01d9772d4a55ee1e4c3fd0855dd2
Michael Lockwood <michael.lockwood@digital.trade.gov.uk>
2024-11-28 09:52:09 +0000

    ECIL-225 Test coverage for admin templates

--- web/domains/template/widgets.py
+++ web/domains/template/widgets.py
@@ -4,5 +4,9 @@ from web.forms.widgets import ICMSModelSelect2Widget
 class EndorsementTemplateWidget(ICMSModelSelect2Widget):
     search_fields = [
         "template_name__icontains",
-        "template_content__icontains",
+        "versions__content__icontains",
     ]
+
+    def filter_queryset(self, request, term, queryset=None, **dependent_fields):
+        qs = super().filter_queryset(request, term, queryset, **dependent_fields)
+        return qs.filter(versions__is_active=True)
