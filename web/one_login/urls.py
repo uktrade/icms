4b46af4c40958b5c87a3a4620b9bf6020dd5e943
Matthew Holmes <matthew.holmes@digital.trade.gov.uk>
2024-12-11 10:22:46 +0000

    ICMSLST-171 Add support for GOV.UK One Login Back Channel Logout.

--- web/one_login/urls.py
+++ web/one_login/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import AuthCallbackView, AuthView
+from .views import AuthCallbackView, AuthView, OIDCBackChannelLogoutView
 
 app_name = "one_login"
 urlpatterns = [
     path("login/", AuthView.as_view(), name="login"),
     path("callback/", AuthCallbackView.as_view(), name="callback"),
+    path("back-channel-logout/", OIDCBackChannelLogoutView.as_view(), name="back-channel-logout"),
 ]
