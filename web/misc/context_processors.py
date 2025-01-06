19e51c8ffd30442568176d2aa23f796af2000a27
Christopher Pettinga <chris.pettinga@digital.trade.gov.uk>
2025-01-06 11:56:43 +0000

    changing color based on site

--- web/misc/context_processors.py
+++ web/misc/context_processors.py
@@ -1,12 +1,21 @@
 from django.conf import settings
 from django.http import HttpRequest
 
+from web.sites import SiteName
+
 
 def environment_information(request: HttpRequest) -> dict[str, str]:
     """Add the current environment & branch to the context."""
+    HEADER_COLOURS = {
+        SiteName.IMPORTER: "green",
+        SiteName.EXPORTER: "grey",
+        SiteName.CASEWORKER: "red",
+    }
+
     return {
         "CURRENT_ENVIRONMENT": settings.CURRENT_ENVIRONMENT,
         "CURRENT_BRANCH": settings.CURRENT_BRANCH,
         "CURRENT_TAG": settings.CURRENT_TAG,
         "SHOW_ENVIRONMENT_INFO": settings.CURRENT_ENVIRONMENT != "prod" and settings.DEBUG,
+        "HEADER_COLOUR": HEADER_COLOURS[request.site.name],
     }
--- web/tests/misc/test_context_processors.py
+++ web/tests/misc/test_context_processors.py
@@ -1,17 +1,40 @@
+from django.contrib.sites.models import Site
 from django.test import RequestFactory, override_settings
 
 from web.misc.context_processors import environment_information
+from web.sites import SiteName
 
 
 @override_settings(
     CURRENT_BRANCH="test-branch", CURRENT_ENVIRONMENT="test", CURRENT_TAG="v1.0.0", DEBUG=True
 )
-def test_environment_information(rf: RequestFactory):
-    assert environment_information(rf.request()) == {
+def test_environment_information(rf: RequestFactory, db):
+    request = rf.request()
+    request.site = Site.objects.get(name=SiteName.IMPORTER)
+    assert environment_information(request) == {
         "CURRENT_ENVIRONMENT": "test",
         "CURRENT_BRANCH": "test-branch",
         "CURRENT_TAG": "v1.0.0",
         "SHOW_ENVIRONMENT_INFO": True,
+        "HEADER_COLOUR": "green",
+    }
+
+    request.site = Site.objects.get(name=SiteName.EXPORTER)
+    assert environment_information(request) == {
+        "CURRENT_ENVIRONMENT": "test",
+        "CURRENT_BRANCH": "test-branch",
+        "CURRENT_TAG": "v1.0.0",
+        "SHOW_ENVIRONMENT_INFO": True,
+        "HEADER_COLOUR": "grey",
+    }
+
+    request.site = Site.objects.get(name=SiteName.CASEWORKER)
+    assert environment_information(request) == {
+        "CURRENT_ENVIRONMENT": "test",
+        "CURRENT_BRANCH": "test-branch",
+        "CURRENT_TAG": "v1.0.0",
+        "SHOW_ENVIRONMENT_INFO": True,
+        "HEADER_COLOUR": "red",
     }
 
 
