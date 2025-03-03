81d7627363d7ab689148ab0e8f7c82a2f120579d
Michael Lockwood <michael.lockwood@digital.trade.gov.uk>
2025-03-03 17:03:56 +0000

    ECIL-620 Separate icms-hmrc and data workspace hawk api creds

--- web/tests/api_auth.py
+++ web/tests/api_auth.py
@@ -1,4 +1,4 @@
-from web.utils.api.auth import HTTPMethod, make_hawk_sender
+from web.utils.api.auth import APIType, HTTPMethod, make_hawk_sender
 
 JSON_TYPE = "application/json"
 # This has to match the request, because it is used to calculate the request's
@@ -6,7 +6,7 @@ JSON_TYPE = "application/json"
 SERVER_NAME = "caseworker"
 
 
-def make_testing_hawk_sender(method: HTTPMethod, url: str, **kwargs):
+def make_testing_hawk_sender(method: HTTPMethod, url: str, api_type: APIType = "hmrc", **kwargs):
     url = f"http://{SERVER_NAME}{url}"
 
-    return make_hawk_sender(method, url, **kwargs)
+    return make_hawk_sender(method, url, api_type=api_type, **kwargs)
